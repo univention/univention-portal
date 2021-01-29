e8b84056b845c1f14d5512659e8d33d5f8e90689
Dirk Wiesenthal <wiesenthal@univention.de>
2021-01-29 22:29:53 +0100

    Issue #21: Tile.vue (and a most simple example)

---
+++ src/components/Tile.vue
@@ -0,0 +1,69 @@
+<template>
+  <a v-bind:href="link" draggable="true">
+    <div class="tile">
+      <div class="box" v-bind:style="'background: ' + backgroundColor">
+        <img v-bind:src="logo" v-bind:alt="title + ' logo'" />
+      </div>
+      <span class="name">{{ title }}</span>
+    </div>
+  </a>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    title: String,
+    link: String,
+    logo: String,
+    backgroundColor: String
+  }
+})
+export default class Tile extends Vue {
+  title!: string;
+  link!: string;
+  logo = "questionMark.svg";
+  backgroundColor = "green";
+}
+</script>
+
+<style scoped lang="stylus">
+a,
+a:hover
+	color: var(--font-color-contrast-high)
+	text-decoration: none
+
+a
+	position: relative
+	outline: 0
+
+.tile
+	position: relative
+	width: var(--app-tile-side-length)
+	display: flex
+	flex-direction: column
+	align-items: center
+	cursor: pointer
+
+.box
+	border-radius: 15%
+	display: flex
+	align-items: center
+	justify-content: center
+	box-shadow: var(--box-shadow)
+	background: var(--color-grey40)
+	width: var(--app-tile-side-length)
+	height: @width
+	margin-bottom: calc(2 * var(--layout-spacing-unit))
+
+img
+	width: 80%
+
+.name
+	text-align: center
+	width: 100%
+	overflow: hidden
+	text-overflow: ellipsis
+	white-space: nowrap
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Tile title="ownCloud" link="https://www.owncloud.com" />
   </div>
 </template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Tile from "@/components/Tile.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    Tile
+  }
+})
+export default class About extends Vue {}
+</script>
