aa49988096cd47345c979e2a2038de7dc5c8d4ba
Dirk Wiesenthal <wiesenthal@univention.de>
2021-01-30 02:06:33 +0100

    fix(portal): Issue #31: Add categories to store object
    Issue #31

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <Category title="Applications" />
+    <Category
+      v-for="(category, index) in categories"
+      :key="index"
+      :title="category.title"
+      :tiles="category.tiles"
+    />
   </div>
 </template>
 <script lang="ts">
@@ -11,6 +16,11 @@ import Category from "@/components/Category.vue"; // @ is an alias to /src
 @Options({
   components: {
     Category
+  },
+  computed: {
+    categories() {
+      return this.$store.state.categories;
+    }
   }
 })
 export default class About extends Vue {}
