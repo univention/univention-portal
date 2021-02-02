a06eb3f5cefdb019eb657917c52e9194fa56df46
Dirk Wiesenthal <wiesenthal@univention.de>
2021-02-02 13:43:45 +0100

    fix(portal): Issue #22: First glimpse on folders
    Issue #22

---
+++ src/components/Folder.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tile">
+    <div class="folder box">
+      <div class="thumbnails">
+        <div
+          v-for="tile in tiles"
+          :key="tile.title"
+          class="thumbnail"
+          :style="'background: ' + tile.backgroundColor"
+        >
+          <img :src="tile.logo" :alt="tile.title + ' logo'" />
+        </div>
+      </div>
+    </div>
+    <span class="name">{{ title }}</span>
+  </div>
+</template>
+<script lang="ts">
+import { Options } from "vue-class-component";
+import Tile from "@/components/Tile.vue";
+
+@Options({
+  components: {
+    Tile
+  },
+  props: {
+    title: String,
+    tiles: Array
+  }
+})
+export default class Folder extends Tile {
+  title!: string;
+  tiles!: [Tile];
+}
+</script>
+<style scoped lang="stylus">
+/*.folder
+  background: rgba(var(--color-grey0-rgb), 0.7);
+*/
+.thumbnail
+  border-radius: 15%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: var(--box-shadow);
+  background: var(--color-grey40);
+
+.thumbnails
+  width: 80%;
+  height: 80%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 8px;
+</style>
