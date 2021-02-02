77ae7174a5dcca708d51304094fb1a70ac668d23
Chrissy Topal <topal@univention.de>
2021-02-02 14:37:14 +0100

    fix(portal): replacing aboutpage with homepage
    Issue #0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="about">
+    <button v-on:click="devEmpty">Empty</button>
+    <button v-on:click="devStandard">Standard</button>
+    <button v-on:click="devFolder">Folder</button>
+    <Category
+      v-for="(category, index) in categories"
+      :key="index"
+      :title="category.title"
+      :tiles="category.tiles"
+    />
   </div>
 </template>
-
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Category from "@/components/Category.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld
+    Category
+  },
+  computed: {
+    categories() {
+      return this.$store.state.categories;
+    }
+  },
+  methods: {
+    devEmpty() {
+      this.$store.commit("devEmpty");
+    },
+    devStandard() {
+      this.$store.commit("devStandard");
+    },
+    devFolder() {
+      this.$store.commit("devFolder");
+    }
   }
 })
-export default class Home extends Vue {}
+export default class About extends Vue {}
 </script>
