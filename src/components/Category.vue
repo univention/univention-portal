11c98e80cc40ec4da0eddcd1158c7ee1824ab6cc
Stefan Blattmann <stefan.blattmann@gmail.com>
2021-02-05 08:14:09 +0100

    * move tooltip to parent component

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -2,7 +2,12 @@
   <div class="category">
     <h2>{{ title }}</h2>
     <div class="tiles">
-      <template v-for="(tile, index) in tiles" :key="index">
+      <div
+        v-for="(tile, index) in tiles"
+        :key="index"
+        @mouseover="showTooltip"
+        @mouseleave="showTooltip"
+      >
         <Tile
           v-if="isTile(tile)"
           :title="tile.title"
@@ -14,23 +19,37 @@
           :title="tile.title"
           :tiles="tile.tiles"
         />
-      </template>
+        <portal-tool-tip
+          v-if="isActive"
+          :title="tile.title"
+          :icon="tile.logo"
+          :description="tile.description"
+        />
+      </div>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import Tile from "components/Tile.vue";
-import Folder from "components/Folder.vue";
+import Tile from "@/components/Tile.vue";
+import Folder from "@/components/Folder.vue";
+
+import PortalToolTip from "@/components/PortalToolTip.vue";
 
 @Options({
   components: {
     Tile,
-    Folder
+    Folder,
+    PortalToolTip,
   },
   props: {
     title: String,
-    tiles: Array
+    tiles: Array,
+  },
+  data() {
+    return {
+      isActive: false,
+    };
   },
   methods: {
     isTile(obj: Tile) {
@@ -38,7 +57,15 @@ import Folder from "components/Folder.vue";
     },
     isFolder(obj: Tile) {
       return obj instanceof Folder;
-    }
+    },
+    showTooltip(): boolean {
+      const handleActive = this.isActive
+        ? (this.isActive = false)
+        : (this.isActive = true);
+
+      console.log('showTooltip: ', this.isActive);
+      return handleActive;
+    },
   }
 })
 export default class Category extends Vue {
