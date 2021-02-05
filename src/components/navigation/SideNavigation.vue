88ee22bf6df729d9f3df1c58d0c2ef2aaa36855a
Dirk Wiesenthal <wiesenthal@univention.de>
2021-02-06 00:38:01 +0100

    fix(portal): Issue #32: User info in Sidenavigation
    Issue #32

--- src/components/navigation/SideNavigation.vue
+++ src/components/navigation/SideNavigation.vue
@@ -1,25 +1,38 @@
 <template>
-  <nav class="navigation">
-    <ul>
-      <li v-if="username">
-        {{ username }}
-      </li>
+  <nav class="portal--sidenavigation">
+    <div class="portal--sidenavigation--login-header">
+      <div v-if="username" class="portal--sidenavigation--user-row">
+        <portal-icon icon="user" iconWidth="6rem" />
+        <div>
+          <div class="portal--sidenavigation--username">{{ username }}</div>
+          <div class="portal--sidenavigation--logout-link" @click="logout">Logout</div>
+        </div>
+      </div>
+      <div v-else class="portal--sidenavigation-link" @click="login">
+        Login
+      </div>
+    </div>
+    <ul class="portal--sidenavigation--menu">
       <li>Item 1</li>
       <li>Item 2</li>
       <li>Item 3</li>
       <li>Item 4</li>
-      <li v-if="isAdmin">
-        Portal bearbeiten
-      </li>
     </ul>
+    <div v-if="isAdmin" class="portal--sidenavigation-link portal--sidenavigation--edit-mode">
+      Portal bearbeiten
+    </div>
   </nav>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import PortalIcon from "@/components/globals/PortalIcon.vue";
 
 @Options({
   name: "SideNavigation",
+  components: {
+    PortalIcon,
+  },
   computed: {
     username(): string {
       return this.$store.state.user.username;
@@ -27,15 +40,62 @@ import { Options, Vue } from "vue-class-component";
     isAdmin(): boolean {
       return this.$store.state.user.isAdmin;
     },
+  },
+  methods: {
+    login() {
+      this.$store.commit("devLogin");
+    },
+    logout() {
+      this.$store.commit("devLogout");
+    }
   }
 })
 export default class SideNavigation extends Vue {}
 </script>
 
 <style lang="stylus">
-.navigation
-  height: calc(100vh - (var(--portal-header-height) + 0.5rem));
+.portal--sidenavigation
+  height: calc(100vh - (var(--portal-header-height) + 0.5rem))
+  display: flex
+  flex-direction: column
+  align-item: flex-end
 
-  a
-    color: #fff;
+.portal--sidenavigation-link
+  padding: 1em 0 1em 20px
+  cursor: pointer
+  height: auto
+  justify-content: left
+  font-size: var(--font-size-normal)
+  color: var(--font-color-contrast-high)
+  font-weight: 600
+  text-transform: uppercase
+  &:hover
+    background-color: #272726
+
+.portal--sidenavigation--user-row
+  display: flex
+
+  svg
+    color: var(--font-color-contrast-high)
+    fill: currentColor
+    background-color: var(--color-grey40)
+    margin: 0.5rem
+    border-radius: 50%
+  &> div
+    margin: auto 0
+
+.portal--sidenavigation--logout-link
+  text-decoration: underline
+  cursor: pointer
+  &:hover
+    background-color: #272726
+
+.portal--sidenavigation--login-header
+  border-bottom: 4px solid var(--color-grey8)
+
+.portal--sidenavigation--menu
+  margin-bottom: auto
+
+.portal--sidenavigation--edit-mode
+  border-top: 4px solid var(--color-grey8)
 </style>
