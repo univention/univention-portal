b5cf46c526181727ff60e0eb0d75284fef350bac
Stefan Blattmann <blattmann@univention.de>
2021-02-08 08:08:28 +0100

    fix(portal): * upgrade tooling package
    Issue #0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,60 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    // 'ecmaFeatures': {
+    //   'legacyDecorators': true,
+    // },
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    // "eslint:recommended",
+    // "plugin:vue/vue3-essential",
+    // "@vue/typescript/recommended",
+    // "@vue/prettier",
+    // "@vue/prettier/@typescript-eslint",
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'airbnb',
+    '@vue/eslint-config-airbnb',
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
+  plugins: [
+    'vue',
+    'json',
+    'jest',
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': ['error', { allow: ['error'] }],
+    'import/extensions': ['error', 'never'],
+    'max-len': 'off',
+    'no-useless-escape': 'off',
+    quotes: [2, 'single', { avoidEscape: true }],
+    'operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
+    'comma-spacing': ['error', {
+      before: false,
+      after: true,
+    }],
+    'space-before-function-paren': ['error', 'never'],
+    'arrow-parens': ['error', 'always'],
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
   },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+  // overrides: [
+  //   {
+  //     files: [
+  //       '**/__tests__/*.{j,t}s?(x)',
+  //       '**/tests/unit/**/*.spec.{j,t}s?(x)'
+  //     ],
+  //     env: {
+  //       jest: true
+  //     }
+  //   }
+  // ]
 };
