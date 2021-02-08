326e14d6c74c10bbb38c08904b03593fabf6c8ca
Stefan Blattmann <blattmann@univention.de>
2021-02-08 21:50:55 +0100

    fix(portal): * add stylus loader
    * remove global css vars from public folder
    * add global stylus files
    
    Issue #0

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,25 @@
 // eslint-disable-next-line
-const path = require("path");
+const path = require('path');
 
 const vueConfig = {
   filenameHashing: false,
   pwa: {
     name: 'UPX Portal',
   },
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+      stylus: {
+        import: [
+          path.resolve(__dirname, 'src/assets/styles/_vars.styl'),
+          path.resolve(__dirname, 'src/assets/styles/_variables.styl'),
+          path.resolve(__dirname, 'src/assets/styles/_base.styl'),
+        ],
+      },
+    },
+  },
 };
 
-// We may already have an existing `configureWebpack` definition (e.g. when building the server bundle).
-// So we need to preserve that definition and ensure it is invoked along with the config
-// options that are common to both client/server bundles.
 const existingConfigureWebpack = vueConfig.configureWebpack;
 
 vueConfig.configureWebpack = (config) => {
@@ -18,36 +27,12 @@ vueConfig.configureWebpack = (config) => {
     existingConfigureWebpack(config);
   }
 
-  // we turn off `.mjs` file support in Vue, because `graphql` ships
-  // mjs files in its npm package (bad) and Vue's webpack settings destroy
-  // graphql's mjs files, causing strange runtime errors. By disabling mjs,
-  // we make webpack use the .js files in graphql instead, which work fine.
-  const indexOfMjs = config.resolve.extensions.indexOf('.mjs');
-  if (indexOfMjs > -1) {
-    config.resolve.extensions.splice(indexOfMjs, 1);
-  }
-  config.module.rules.push({
-    test: /\.(graphql|gql)$/,
-    exclude: /node_modules/,
-    loader: 'graphql-tag/loader',
-  });
-
   config.module.rules.push({
     test: /\.ya?ml$/,
     loader: 'json-loader!yaml-loader',
   });
 };
 
-// Below is a workaround when building the app within the context of the JSS monorepo.
-// The monorepo uses symlinks within the `node_modules` folder to reference `sitecore-jss-*` packages,
-// which causes the eslint loader to attempt resolving eslint config from the _actual_ package location,
-// not the "virtual" location under `node_modules`. In turn, an incorrect eslint config is resolved and
-// breaks the build process.
-// The workaround is to exclude the `packages/sitecore-jss*` packages from eslint-loader.
-
-// We may already have an existing `chainWebpack` definition (e.g. when building the server bundle).
-// So we need to preserve that definition and ensure it is invoked along with the config
-// options that are common to both client/server bundles.
 const existingChainWebpack = vueConfig.chainWebpack;
 vueConfig.chainWebpack = (config) => {
   if (existingChainWebpack) {
@@ -67,7 +52,7 @@ vueConfig.chainWebpack = (config) => {
     .set('mixins', path.resolve(__dirname, 'src/mixins'))
     .set('views', path.resolve(__dirname, 'src/views'));
 
-  // TODO: In case that the paths will not be resolved in production we might have to install "module-alias": https://medium.com/@caludio/how-to-use-module-path-aliases-in-visual-studio-typescript-and-javascript-e7851df8eeaa
+  // TODO: In case that the paths will not be resolved in production we might have to install 'module-alias': https://medium.com/@caludio/how-to-use-module-path-aliases-in-visual-studio-typescript-and-javascript-e7851df8eeaa
 };
 
 module.exports = vueConfig;
