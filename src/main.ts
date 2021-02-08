32621e6ff00e21fbc15e9abd4f831ec8e2aee484
Stefan Blattmann <blattmann@univention.de>
2021-02-08 08:44:33 +0100

    fix(portal): * update file paths
    Issue #0

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
-import App from './App';
-import router from './router';
-import { store } from './store';
+import App from '@/App';
+import router from '@/router';
+import { store } from '@/store';
 
 createApp(App)
   .use(store)
