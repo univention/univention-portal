170b9c5057a04ecb3aa167c011584e2c2fd19123
Stefan Blattmann <blattmann@univention.de>
2021-02-11 22:28:42 +0100

    * add notification bubble to notification panel * add mixin to control notification handling

--- src/components/globals/NotificationBubble.vue
+++ src/components/globals/NotificationBubble.vue
@@ -1,97 +1,26 @@
 <template>
-  <teleport to="body">
+  <div>
     <transition name="fade">
-      <div
+      <slot
         v-if="bubbleState"
-        class="notification-bubble"
-      >
-        <div
-          :class="`notification-bubble__${bubbleContainer}`"
-          class="notification-bubble__container"
-        >
-          <div class="notification-bubble__header">
-            <div class="notification-bubble__title">
-              {{ bubbleContent.bubbleTitle }}
-            </div>
-
-            <header-button
-              :aria-label="bubbleContent.bubbleLabel"
-              :icon="bubbleContent.bubbleIcon"
-              @click="dismissBubble(`${localNotification}`)"
-            />
-          </div>
-
-          <div class="notification-bubble__content">
-            <!-- eslint-disable vue/no-v-html -->
-            <div
-              v-if="bubbleContent && bubbleContent.bubbleDescription"
-              ref="bubbleContent"
-              class="notification-bubble__message"
-              @click="handleClick"
-              v-html="bubbleContent.bubbleDescription"
-            />
-            <!-- eslint-enable vue/no-v-html -->
-          </div>
-        </div>
-      </div>
+        name="bubble-standalone"
+      />
     </transition>
-  </teleport>
+
+    <slot name="bubble-embedded" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import { mapGetters } from 'vuex';
 
-import HeaderButton from '@/components/navigation/HeaderButton.vue';
-
-import userMixin from '@/mixins/userMixin.vue';
-
 @Options({
   name: 'NotificationBubble',
-  components: {
-    HeaderButton,
-  },
-  mixins: [userMixin],
-  props: {
-    bubbleContainer: {
-      type: String,
-      default: 'embedded',
-    },
-  },
-  created() {
-    this.setContent();
-  },
   computed: {
     ...mapGetters({
       bubbleState: 'notificationBubble/bubbleState',
-      bubbleContent: 'notificationBubble/bubbleContent',
     }),
-  },
-  methods: {
-    dismissBubble() {
-      if (this.bubbleState) {
-        // store modal state
-        this.$store.dispatch('notificationBubble/setHideBubble');
-      }
-    },
-    setContent() {
-      // TODO: replce with dynamic content from e.g. an API
-      const payload = {
-        bubbleIcon: 'x',
-        bubbleLabel: 'Dismiss notification',
-        bubbleType: 'localNotification',
-        bubbleTitle: 'Login',
-        bubbleDescription: 'Login <a class="notification-bubble__link" href="#">here</a> so that you can use the full range of functions of UCS.',
-      };
-
-      this.$store.dispatch('notificationBubble/setContent', payload);
-    },
-    handleClick(e) {
-      if (e.target.matches('.notification-bubble__link, .notification-bubble__link *')) {
-        this.dismissBubble();
-        console.info('Bubble link clicked - TODO: add some action');
-      }
-    },
   },
 })
 
@@ -104,7 +33,6 @@ export default class NotificationBubble extends Vue {}
 
   &__container
     max-width: 288px;
-    // background-color: #aaa;
     background-color: rgba(0,0,0,0.4);
     backdrop-filter: blur(20px);
     border-radius: var(--border-radius-notification);
@@ -119,34 +47,14 @@ export default class NotificationBubble extends Vue {}
     margin: 0;
 
   &__embedded
-
-  &__header
-    display: flex;
-    align-items: center;
-    margin-bottom: 8px;
-
-  &__title
-    flex: 1 1 auto;
-
-  &__content
-    padding: 0 !important;
-    display: block;
-    overflow: auto;
-
-  &__message
-    text-decoration: none
-
-    &>a
-      color: #fff!important;
-      transition: color 250ms;
-      text-decoration: underline;
+    position: relative
 
 // animation
 .fade-enter-active,
 .fade-leave-active
-  transition: opacity .5s;
+  transition: opacity .5s
 
 .fade-enter,
 .fade-leave-to
-  opacity: 0;
+  opacity: 0
 </style>
---
+++ src/components/globals/NotificationBubbleSlot.vue
@@ -0,0 +1,111 @@
+<template>
+  <div :class="`notification-bubble__${bubbleContainer}`">
+    <div class="notification-bubble__container">
+      <div>
+        <div class="notification-bubble__header">
+          <div class="notification-bubble__title">
+            {{ bubbleContent.bubbleTitle }}
+          </div>
+
+          <header-button
+            :aria-label="bubbleContent.bubbleLabel"
+            :icon="bubbleContent.bubbleIcon"
+            @click="dismissBubble(`${localNotification}`)"
+          />
+        </div>
+
+        <div class="notification-bubble__content">
+          <!-- eslint-disable vue/no-v-html -->
+          <div
+            v-if="bubbleContent && bubbleContent.bubbleDescription"
+            ref="bubbleContent"
+            class="notification-bubble__message"
+            @click="bubbleClick"
+            v-html="bubbleContent.bubbleDescription"
+          />
+          <!-- eslint-enable vue/no-v-html -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { mapGetters } from 'vuex';
+
+import HeaderButton from '@/components/navigation/HeaderButton.vue';
+
+import userMixin from '@/mixins/userMixin.vue';
+import notificationMixin from '@/mixins/notificationMixin.vue';
+
+@Options({
+  name: 'NotificationBubbleSlot',
+  components: {
+    HeaderButton,
+  },
+  mixins: [
+    userMixin,
+    notificationMixin,
+  ],
+  props: {
+    bubbleContainer: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    ...mapGetters({
+      bubbleContent: 'notificationBubble/bubbleContent',
+    }),
+  },
+})
+
+export default class NotificationBubbleSlot extends Vue {}
+</script>
+
+<style lang="stylus">
+.notification-bubble
+  &__standalone
+    max-width: 320px
+    position: absolute
+    right: 20px
+    top: 8rem
+    z-index: 10
+
+  &__embedded
+    max-width: 320px
+    position: relative
+    right: 0
+    top: 0
+
+  &__container
+    max-width: 288px
+    background-color: rgba(0,0,0,0.4)
+    backdrop-filter: blur(20px)
+    border-radius: var(--border-radius-notification)
+    padding: 16px
+    font-size: 16px
+    margin-bottom: 16px
+
+  &__header
+    display: flex
+    align-items: center
+    margin-bottom: 8px
+
+  &__title
+    flex: 1 1 auto
+
+  &__content
+    padding: 0 !important
+    display: block
+    overflow: auto
+
+  &__message
+    text-decoration: none
+
+    &>a
+      color: #fff!important
+      transition: color 250ms
+      text-decoration: underline
+</style>
