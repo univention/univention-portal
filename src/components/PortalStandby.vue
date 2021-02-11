d4888685d0e6828dcbdb9e0082eae04718039808
Stefan Blattmann <stefan.blattmann@gmail.com>
2021-02-11 22:49:39 +0100

    *  fix css

--- src/components/PortalStandby.vue
+++ src/components/PortalStandby.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="wrapper">
-    <div class="lds-ring">
-      <div />
-      <div />
-      <div />
-      <div />
+  <div class="portal-standby">
+    <div class="portal-standby__lds-ring">
+      <div class="portal-standby__ring-item" />
+      <div class="portal-standby__ring-item" />
+      <div class="portal-standby__ring-item" />
+      <div class="portal-standby__ring-item" />
     </div>
   </div>
 </template>
 
 <style scoped lang="stylus">
-.wrapper
+.portal-standby
   position: absolute
   top: 0
   left: 0
@@ -18,15 +18,15 @@
   width: 100%
   height: 100%
 
-.lds-ring
-  display: inline-block
-  position: relative
-  width: 80px
-  height: 80px
-  top: 45%
-  left: 45%
+  &__lds-ring
+    display: inline-block
+    position: relative
+    width: 80px
+    height: 80px
+    top: 45%
+    left: 45%
 
-  div
+  &__ring-item
     box-sizing: border-box
     display: block
     position: absolute
