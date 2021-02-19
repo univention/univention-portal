0d5a961e8efc9649fbacef2e490f6c0980cafb8e
Dirk Wiesenthal <wiesenthal@univention.de>
2021-02-19 14:30:09 +0100

    feat: Debian Package for UCS 5 [#52]

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <router-view />
+  <home />
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import { Options, Vue } from 'vue-class-component';
 import { store } from '@/store';
+import Home from '@/views/Home.vue';
 
-export default class App extends Vue {
-  store: typeof store = store;
+@Options({
+  name: 'App',
+
+  components: {
+    Home,
+  },
   /* eslint-disable */
   mounted() {
     store.dispatch('loadPortal');
   }
   /* eslint-enable */
+})
+
+export default class App extends Vue {
+  store: typeof store = store;
 }
 </script>
