028fcce831195fe0d06c9aa1817514bd2ca6bf01
Dirk Wiesenthal <wiesenthal@univention.de>
2021-03-03 23:44:22 +0100

    fix: Correct click behaviour on menu items
    Issue #81

---
+++ src/mixins/TileClick.vue
@@ -0,0 +1,63 @@
+<script lang="ts">
+import { mapGetters } from 'vuex';
+import bestLink from '@/jsHelper/bestLink.js';
+
+const tileClickMixin = {
+  props: {
+    links: {
+      type: Array,
+      required: true,
+    },
+    linkTarget: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapGetters({
+      metaData: 'meta/getMeta',
+      editMode: 'portalData/editMode',
+    }),
+    link(): string {
+      return bestLink(this.links, this.metaData.fqdn);
+    },
+  },
+  emits: [
+    'clickAction',
+  ],
+  methods: {
+    tileClick(evt) {
+      if (this.link === null) {
+        return false;
+      }
+      if (this.editMode) {
+        evt.preventDefault();
+
+        // TODO: start edit tile dialog
+        return false;
+      }
+      if (this.inFolder) {
+        evt.preventDefault();
+        return false;
+      }
+      this.$emit('clickAction');
+      if (this.linkTarget === 'embedded') {
+        evt.preventDefault();
+        this.openEmbedded();
+        return false;
+      }
+      return true;
+    },
+    openEmbedded() {
+      const tab = {
+        tabLabel: this.$localized(this.title),
+        logo: this.pathToLogo,
+        iframeLink: this.link,
+      };
+      this.$store.dispatch('tabs/addTab', tab);
+    },
+  },
+};
+
+export default tileClickMixin;
+</script>
