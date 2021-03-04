80cb72182c993820d463d9279a2d5cd237d3ca9d
Denis Drescher <drescher@univention.de>
2021-03-04 13:58:10 +0100

    chore: Refactor files based on auto linting fix
    Issue #64

--- .eslintrc.js
+++ .eslintrc.js
@@ -61,7 +61,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    // 'import/extensions': ['error', 'never'],
+    'import/extensions': ['error', 'never'],
     // 'import/extensions': [
     //   'error',
     //   'ignorePackages',
