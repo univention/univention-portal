65eccf8f9aa1568ed9fb1d8c2d24e41fd59bf9a2
Denis Drescher <drescher@univention.de>
2021-03-05 01:13:30 +0100

    chore: Add further linting rules to purify setup
    Issue #64

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,21 +30,7 @@ module.exports = {
     '@vue/typescript/recommended',
   ],
   rules: {
-    'max-len': [
-      'error',
-      {
-        code: 120,
-        ignoreComments: true,
-        ignoreUrls: true,
-      },
-    ],
-    'no-useless-escape': 'off',
-    'quotes': ['error', 'single', { avoidEscape: true }],
-    'operator-linebreak': [
-      'error',
-      'after',
-      { overrides: { '?': 'before', ':': 'before' } },
-    ],
+    'arrow-parens': ['error', 'always'],
     'comma-spacing': [
       'error',
       {
@@ -52,27 +38,39 @@ module.exports = {
         after: true,
       },
     ],
-    'space-in-parens': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'space-before-function-paren': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'arrow-parens': ['error', 'always'],
+    // 'import/extensions': ['error', 'always', { ignorePackages: true }],
+    'max-len': [
+      'error',
+      {
+        code: 180,
+        ignoreComments: true,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreUrls: true,
+      },
+    ],
     'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
-    'prefer-destructuring': 'off',
-    'quote-props': ['error', 'consistent-as-needed'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-param-reassign': ['error', { props: false }],
     'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'import/extensions': ['error', 'never'],
-    // 'import/extensions': [
-    //   'error',
-    //   'ignorePackages',
-    //   {
-    //     js: 'never',
-    //     jsx: 'never',
-    //     ts: 'never',
-    //     tsx: 'never',
-    //     vue: 'never',
-    //   },
-    // ],
+    'no-useless-escape': 'off',
+    'operator-linebreak': [
+      'error',
+      'after',
+      { overrides: { '?': 'before', ':': 'before' } },
+    ],
+    'prefer-destructuring': 'off',
+    'quote-props': ['error', 'consistent-as-needed'],
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    'space-in-parens': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'space-before-function-paren': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+
+    // ToDo: Temporay rule settings that should be deleted when code issues are fixed!
+
+    'import/no-extraneous-dependencies': 'warn', // eg no use of 'require' -> Convert all .js to .ts files and use import!
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   overrides: [
     {
