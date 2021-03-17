7ebb5f0b77ebf23fe5352fdf6996b0b732eae17a
ctopal <topal@univention.de>
2021-03-17 12:41:31 +0100

    changing importsrc

--- src/components/globals/NotificationBubbleSlot.vue
+++ src/components/globals/NotificationBubbleSlot.vue
@@ -46,7 +46,7 @@
 import { defineComponent } from 'vue';
 
 import HeaderButton from '@/components/navigation/HeaderButton.vue';
-import notificationMixin from '@/mixins/notificationMixin.vue';
+import notificationMixin from '@/mixins/notificationMixin';
 
 import { catalog } from '@/i18n/translations';
 
