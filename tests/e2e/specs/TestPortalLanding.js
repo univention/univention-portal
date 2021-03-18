c8a83eaacfe2228d47e1cc6362f1b6a2f6be696c
Andreas Nolden <nolden@univention.de>
2021-03-18 19:07:12 +0100

    fix: added comment
    Issue #59

--- tests/e2e/specs/TestPortalLanding.js
+++ tests/e2e/specs/TestPortalLanding.js
@@ -2,6 +2,7 @@
 
 describe('General Tests', () => {
   it('Loads the page', () => {
+    // stuff selenium can't do #1: mock requests / responses
     cy.intercept('GET', 'portal/portal.json', { fixture: 'portal.json' });
     cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
     cy.intercept('GET', 'de.json', { fixture: 'de.json' });
