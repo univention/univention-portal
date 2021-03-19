b652c336f0143164b09868b7900b410d3cc82261
Dirk Wiesenthal <wiesenthal@univention.de>
2021-03-19 09:33:03 +0100

    fix: Use the dedicated language.json from UCS [#96]

--- src/store/index.ts
+++ src/store/index.ts
@@ -18,6 +18,7 @@ import { RootState } from './types';
 
 // get env vars
 const portalUrl = process.env.VUE_APP_PORTAL_URL || '';
+const languageJsonPath = process.env.VUE_APP_LANGUAGE_DATA || '/univention/languages.json';
 const portalJsonPath = process.env.VUE_APP_PORTAL_DATA || './portal.json';
 const portalMetaPath = process.env.VUE_APP_META_DATA || '/univention/meta.json';
 
@@ -50,12 +51,13 @@ export const store = createStore<RootState>({
       const portalPromises = [
         `${portalUrl}${portalMetaPath}`, // Get meta data
         `${portalUrl}${portalJsonPath}`, // Get portal data
+        `${portalUrl}${languageJsonPath}`, // Get locale data
       ].map((url) => axios.get(url));
 
-      axios.all(portalPromises).then(axios.spread((metaResponse, portalResponse) => {
-        const [meta, portal] = [metaResponse.data, portalResponse.data];
+      axios.all(portalPromises).then(axios.spread((metaResponse, portalResponse, languageResponse) => {
+        const [meta, portal, availableLocales] = [metaResponse.data, portalResponse.data, languageResponse.data];
         dispatch('metaData/setMeta', meta);
-        dispatch('menu/setMenu', portal);
+        dispatch('menu/setMenu', { portal, availableLocales });
         dispatch('portalData/setPortal', portal);
         dispatch('categories/setOriginalArray', portal);
         dispatch('user/setUser', {
