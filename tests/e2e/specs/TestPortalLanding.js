d1845bae027eb6e2adad49c1a3a70614aabcc10b
Andreas Nolden <nolden@univention.de>
2021-03-19 13:56:34 +0100

    fix: more testing[#13]

--- tests/e2e/specs/TestPortalLanding.js
+++ tests/e2e/specs/TestPortalLanding.js
@@ -7,6 +7,19 @@ describe('General Tests', () => {
     cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
     cy.intercept('GET', 'de.json', { fixture: 'de.json' });
     cy.visit('/');
+    /**
+     * Since there are few data-test attributes the selectors are all over the place,
+     * this is quite common but not a best practice. So this is good for learning but
+     * should be refactored.
+     */
     cy.contains('h2', 'Verwaltung');
+    // Tiles?
+    cy.get('.portal-tile__name').contains('span', 'Univention Blog');
+    // Login notification?
+    cy.get('.notification-bubble__title').contains('Anmelden');
+    // Buttons ?
+    cy.get('button[aria-label="Button for Searchbar"]');
+    cy.get('button[aria-label="Open notifications"]');
+    cy.get('button[aria-label="Button for navigation"]');
   });
 });
