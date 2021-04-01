9fc91f2141e8045fe4fd7532fb116ce721f5c092
ctopal <topal@univention.de>
2021-04-01 12:50:25 +0200

    writing search test for tooltip [#66], [#138]

--- tests/e2e/specs/TestSearch.js
+++ tests/e2e/specs/TestSearch.js
@@ -15,23 +15,56 @@ describe('General Tests', () => {
     cy.getCookie('univentionCookieSettingsAccepted').should('exist');
 
 
+    // make inputfield visible 
     const searchButton = cy.get('[data-test="searchbutton"]');
-    
     searchButton.should('not.have.class', 'header-button--is-active');
-  
     cy.get('[data-test="searchInput"]').should('not.be.visible'); // input exists after searchButton is clicked
-    
     cy.get('[data-test="searchbutton"]').click();
     searchButton.should('have.class', 'header-button--is-active');
     cy.get('[data-test="searchInput"]').should('exist');
-    cy.get('[data-test="searchInput"]');
 
+    // test for tilename
     cy.contains('Handbuch');
     cy.get('[data-test="searchInput"]').type('Blog');
     cy.contains('Handbuch').should('not.exist');
     cy.contains('Blog');
   });
+
+
   it('searches also for tile description', () => {
-      // how to write test for description? Tooltip is not redered. 
+    // General Stuff, has nothing to do with search
+    cy.setCookie('UMCLang', 'de_DE');
+    // stuff selenium can't do #1: mock requests / responses
+    cy.intercept('GET', 'portal.json', { fixture: 'portal_logged_out.json' });
+    // cy.intercept('GET', 'portal/portal.json', { fixture: 'portal_logged_in.json' });
+    cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
+    cy.intercept('GET', 'de.json', { fixture: 'de.json' });
+    cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
+    cy.visit('/');
+    cy.contains('Cookie-Einstellungen');
+    cy.get('.cookie-banner__button-text').click();
+    cy.getCookie('univentionCookieSettingsAccepted').should('exist');
+
+
+    // make inputfield visible 
+    const searchButton = cy.get('[data-test="searchbutton"]');
+    searchButton.should('not.have.class', 'header-button--is-active');
+    cy.get('[data-test="searchInput"]').should('not.be.visible'); // input exists after searchButton is clicked
+    cy.get('[data-test="searchbutton"]').click();
+    searchButton.should('have.class', 'header-button--is-active');
+    cy.get('[data-test="searchInput"]').should('exist');
+
+
+  // test for query of description
+  // before typing text of tooltip is decsription of first tile
+  cy.get('.portal-tile').first().trigger('mouseover');
+  cy.get('[data-test="portal-tooltip"]').contains('Cloud Lösung für Filesync und -share');
+  cy.get('#ownCloud').trigger('mouseleave');
+  cy.get('[data-test="portal-tooltip"]').should('not.exist');
+
+  // typing text from second tile (not folder)
+  cy.get('[data-test="searchInput"]').type('Univention Management Console zur Ver­wal­tung der UCS-Domäne und des lokalen Systems');
+  cy.get('.portal-tile').first().trigger('mouseover');
+  cy.get('[data-test="portal-tooltip"]').contains('Univention Management Console zur Ver­wal­tung der UCS-Domäne und des lokalen Systems');
   });
 });
