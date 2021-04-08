b2906ecc9d3ec3cee1100eac2cc3341ed3ba672a
ctopal <topal@univention.de>
2021-04-08 11:11:20 +0200

    fix(portal): adding cookiemanagement in beforeEach-Hook
    Issue #66

--- tests/e2e/specs/TestSearch.js
+++ tests/e2e/specs/TestSearch.js
@@ -1,20 +1,20 @@
 // Iframes are inherently unsafe. Don't use them.
+beforeEach(() => {
+  cy.setCookie('UMCLang', 'de_DE');
+  // stuff selenium can't do #1: mock requests / responses
+  cy.intercept('GET', 'portal.json', { fixture: 'portal_logged_out.json' });
+  // cy.intercept('GET', 'portal/portal.json', { fixture: 'portal_logged_in.json' });
+  cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
+  cy.intercept('GET', 'de.json', { fixture: 'de.json' });
+  cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
+  cy.visit('/');
+  cy.contains('Cookie-Einstellungen');
+  cy.get('.cookie-banner__button-text').click();
+  cy.getCookie('univentionCookieSettingsAccepted').should('exist');
+});
 
 describe('General Tests', () => {
   it('search shows results with "Blog"', () => {
-    cy.setCookie('UMCLang', 'de_DE');
-    // stuff selenium can't do #1: mock requests / responses
-    cy.intercept('GET', 'portal.json', { fixture: 'portal_logged_out.json' });
-    // cy.intercept('GET', 'portal/portal.json', { fixture: 'portal_logged_in.json' });
-    cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
-    cy.intercept('GET', 'de.json', { fixture: 'de.json' });
-    cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
-    cy.visit('/');
-    cy.contains('Cookie-Einstellungen');
-    cy.get('.cookie-banner__button-text').click();
-    cy.getCookie('univentionCookieSettingsAccepted').should('exist');
-
-
     // make inputfield visible 
     const searchButton = cy.get('[data-test="searchbutton"]');
     searchButton.should('not.have.class', 'header-button--is-active');
@@ -32,20 +32,6 @@ describe('General Tests', () => {
 
 
   it('searches also for tile description', () => {
-    // General Stuff, has nothing to do with search
-    cy.setCookie('UMCLang', 'de_DE');
-    // stuff selenium can't do #1: mock requests / responses
-    cy.intercept('GET', 'portal.json', { fixture: 'portal_logged_out.json' });
-    // cy.intercept('GET', 'portal/portal.json', { fixture: 'portal_logged_in.json' });
-    cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
-    cy.intercept('GET', 'de.json', { fixture: 'de.json' });
-    cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
-    cy.visit('/');
-    cy.contains('Cookie-Einstellungen');
-    cy.get('.cookie-banner__button-text').click();
-    cy.getCookie('univentionCookieSettingsAccepted').should('exist');
-
-
     // make inputfield visible 
     const searchButton = cy.get('[data-test="searchbutton"]');
     searchButton.should('not.have.class', 'header-button--is-active');
