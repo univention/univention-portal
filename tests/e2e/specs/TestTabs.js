46ad0a947956af4d48c49481128b76bcb4c7b218
ctopal <topal@univention.de>
2021-07-02 16:27:32 +0300

    fix(portal): test closing tab
    Issue #248

--- tests/e2e/specs/TestTabs.js
+++ tests/e2e/specs/TestTabs.js
@@ -11,43 +11,32 @@ describe('General Tests', () => {
       cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
       cy.setCookie('univentionCookieSettingsAccepted', 'doesthisneedavalue');
       cy.visit('/');
+      // first click results to first tab and first Iframe (first element in array)
       cy.get('.portal-category .portal-tile').last().click();
-      // cypress can't look into frames out of the box
-      cy.get('iframe').should('be.visible');
+      cy.get('#iframe-1').should('be.visible');
+      // click to portal
       cy.get('.portal-header__portal-name').click();
       cy.get('#portalCategories').should('be.visible');
       cy.get('iframe').should('not.be.visible');
+      // click to first tab expects to have visible iframe
       cy.get('#headerTab__1').click();
       cy.get('iframe').should('be.visible');
+      // go back to portal to open second tab
       cy.get('.portal-header__portal-name').click();
       cy.get('#portalCategories').should('be.visible');
       cy.get('[href="https://doc.owncloud.com/server/10.0/admin_manual/"]').click();
       cy.get('iframe').should('be.visible');
+      // now we have two tabs and can switch between them
       cy.get('[data-test="header-tabs"]').children().should('have.length', 2)
       cy.get('#headerTab__1').click();
+      cy.get('#iframe-1').should('be.visible');
       cy.get('iframe').should('be.focused');
-      cy.get('[data-test="portal-iframes"]').children().first().should('be.visible');
-      // cy.get('#close-tab-1').click();
+      // closing last tab
+      cy.get('#headerTab__2').click();
+      cy.get('#iframe-2').should('be.visible');
+      cy.get('#headerTab__2').children().last().click();
+      cy.get('#portalCategories').should('be.visible');
+
     });
   });
-  // it('shows Iframe Tabs', () => {
-  //   // stuff selenium can't do #2: mock requests / responses
-  //   const portal = cy.readFile('public/data/portal.json').then((portal) => {
-  //     portal.entries[0].linkTarget = 'samewindow';
-  //     cy.intercept('GET', 'portal.json', portal);
-  //     cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
-  //     cy.intercept('GET', 'de.json', { fixture: 'de.json' });
-  //     cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
-  //     cy.setCookie('univentionCookieSettingsAccepted', 'doesthisneedavalue');
-  //     cy.visit('/');
-  //     // cy.get('#ownCloud a').
-  //     // // cypress can't look into frames out of the box
-  //     // cy.get('iframe').should('be.visible');
-  //     // cy.get('.portal-header__portal-name').click();
-  //     // cy.get('iframe').should('not.be.visible');
-  //     // cy.get('#headerTab__1').click();
-  //     // cy.get('iframe').should('be.visible');
-  //     // cy.get('#header-button-x').click();
-  //   });
-  // });
 });
