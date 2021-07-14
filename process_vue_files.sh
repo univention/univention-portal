5e903173069ae830bdbec0da38b9b8833ec2985f
ctopal <topal@univention.de>
2021-07-14 12:51:36 +0200

    fix(portal): fix process_vue_files.sh
    Issue #172

--- process_vue_files.sh
+++ process_vue_files.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -r src/tmp/ || true
 mkdir src/tmp/
-for f in $(ls src/components/*.vue src/components/*/*.vue); do
+for f in $(ls src/views/*.vue src/components/*.vue src/components/*/*.vue); do
 	mkdir -p "src/tmp/ts/$(dirname "$f")"
 	sed -n '/^<script/,/^<\/script/p' "$f" | sed '1d;$ d' > "src/tmp/ts/$f"
 	mkdir -p "src/tmp/html/$(dirname "$f")"
