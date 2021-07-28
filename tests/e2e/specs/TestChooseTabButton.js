f4719c1bd595e8124a578027bcaa4f09886a7387
ctopal <topal@univention.de>
2021-07-28 12:18:05 +0200

    fix(portal): [ #253] write new test for choose tabButton component
    Issue #253

---
+++ tests/e2e/specs/TestChooseTabButton.js
@@ -0,0 +1,18 @@
+// Iframes are inherently unsafe. Don't use them.
+
+describe('ChooseTabButton Component', () => {
+    it('ChooseTabButton.vue works correctly', () => {
+        cy.intercept('GET', 'portal.json', { fixture: 'portal_choose_tab_button.json' });
+        portal.entries[0].linkTarget = 'embedded';
+        cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
+        cy.intercept('GET', 'de.json', { fixture: 'de.json' });
+        cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
+        cy.setCookie('univentionCookieSettingsAccepted', 'doesthisneedavalue');
+        cy.viewport(1024, 768);
+        cy.visit('/');
+        // first click results to first tab and first Iframe (first element in array)
+        const testLinks = cy.get('a[href*="wikipedia"]');
+        testLinks.should('have.length', 6)
+    });
+  });
+  
