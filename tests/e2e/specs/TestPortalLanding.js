73388c6a02963d297d137f4c1f26eea21617e77a
ctopal <topal@univention.de>
2021-09-01 15:45:08 +0200

    [#120] add a11y test

--- tests/e2e/specs/TestPortalLanding.js
+++ tests/e2e/specs/TestPortalLanding.js
@@ -1,37 +1,40 @@
-// https://docs.cypress.io/api/introduction/api.html
+import 'cypress-axe';
+
+beforeEach(() => {
+  cy.clearCookie('univentionCookieSettingsAccepted');
+  cy.setCookie('UMCLang', 'de_DE');
+  // stuff selenium can't do #1: mock requests / responses
+  cy.intercept('GET', 'portal.json', { fixture: 'portal_logged_out.json' });
+  // cy.intercept('GET', 'portal/portal.json', { fixture: 'portal_logged_in.json' });
+  cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
+  cy.intercept('GET', 'de.json', { fixture: 'de.json' });
+  cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
+  cy.visit('/');
+  cy.get('main.cookie-banner + footer button.primary').click();
+});
 
 describe('General Tests', () => {
-  it('Loads the page', () => {
-    cy.clearCookie('univentionCookieSettingsAccepted');
-    cy.setCookie('UMCLang', 'de_DE');
-    // stuff selenium can't do #1: mock requests / responses
-    cy.intercept('GET', 'portal.json', { fixture: 'portal_logged_out.json' });
-    // cy.intercept('GET', 'portal/portal.json', { fixture: 'portal_logged_in.json' });
-    cy.intercept('GET', 'meta.json', { fixture: 'meta.json' });
-    cy.intercept('GET', 'de.json', { fixture: 'de.json' });
-    cy.intercept('GET', 'languages.json', { fixture: 'languages.json' });
-    cy.visit('/');
-    /**
-         * Since there are few data-test attributes the selectors are all over the place,
-         * this is quite common but not a best practice. So this is good for learning but
-         * should be refactored.
-         */
-
-    // cy.contains('Cookie-Einstellungen');
-    cy.get('main.cookie-banner + footer button.primary').click();
-    cy.getCookie('univentionCookieSettingsAccepted').should('exist');
-
+  it('General loading of tiles and menu', () => {
     cy.contains('h2', 'Verwaltung');
     // Tiles?
     cy.get('.portal-tile__name').contains('span', 'Univention Blog');
 
+    cy.get('[data-test="navigationbutton"]').click();
+    cy.wait(500);
+    cy.get('.portal-sidenavigation__link').contains('Anmelden');
+    cy.contains('Zertifikate');
+    cy.contains('Apps');
+    cy.contains('Hilfe');
+  });
 
+  it('Mouseover is working', () => {
     // Mouseover tooltip?
     cy.get('.portal-category .portal-tile').first().trigger('mouseenter');
     cy.get('[data-test="portal-tooltip"]').contains('ownCloud');
     cy.get('.portal-category .portal-tile').first().trigger('mouseleave');
     cy.get('[data-test="portal-tooltip"]').should('not.exist');
-
+  });
+  it('Headerbuttons become green', () => {
     // Buttons, check if they become green
     const searchbutton = cy.get('[data-test="searchbutton"]');
     searchbutton.should('not.have.class', 'header-button--is-active');
@@ -48,12 +51,21 @@ describe('General Tests', () => {
     menubutton.click();
     menubutton.should('have.class', 'header-button--is-active');
     cy.get('.modal-wrapper--isVisible').click();
-
-    cy.get('[data-test="navigationbutton"]').click();
-    cy.wait(500);
-    cy.get('.portal-sidenavigation__link').contains('Anmelden');
-    cy.contains('Zertifikate');
-    cy.contains('Apps');
-    cy.contains('Hilfe');
+  });
+  it('Cookie should exist', () => {
+    cy.getCookie('univentionCookieSettingsAccepted').should('exist');
+  });
+  it('General a11y test', () => {
+    cy.injectAxe();
+    cy.checkA11y('body', 
+    {
+      runOnly: {
+        type: 'tag',
+        values: ['wcag21aa'],
+      }
+    },
+    cy.terminalLog, {
+      skipFailures: true
+    });
   });
 });
