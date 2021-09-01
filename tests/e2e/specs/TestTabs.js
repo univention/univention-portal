d3d628bdd463e1cb9a6bac1a79e0d5db07852c3e
ctopal <topal@univention.de>
2021-09-01 18:25:14 +0200

    [#120] add license comment

--- tests/e2e/specs/TestTabs.js
+++ tests/e2e/specs/TestTabs.js
@@ -1,6 +1,35 @@
-// Iframes are inherently unsafe. Don't use them.
+/*
+  Copyright 2021 Univention GmbH
 
-describe('General Tests', () => {
+  https://www.univention.de/
+
+  All rights reserved.
+
+  The source code of this program is made available
+  under the terms of the GNU Affero General Public License version 3
+  (GNU AGPL V3) as published by the Free Software Foundation.
+
+  Binary versions of this program provided by Univention to you as
+  well as other copyrighted, protected or trademarked materials like
+  Logos, graphics, fonts, specific documentations and configurations,
+  cryptographic keys etc. are subject to a license agreement between
+  you and Univention and not subject to the GNU AGPL V3.
+
+  In the case you use this program under the terms of the GNU AGPL V3,
+  the program is provided in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public
+  License with the Debian GNU/Linux or Univention distribution in file
+  /usr/share/common-licenses/AGPL-3; if not, see
+  <https://www.gnu.org/licenses/>.
+*/
+
+import 'cypress-axe';
+
+describe('Test Tabs', () => {
   it('shows Iframe Tabs', () => {
     // TODO: Same origin html fake for linktarget tests
     cy.readFile('public/data/portal.json').then((portal) => {
@@ -82,6 +111,20 @@ describe('General Tests', () => {
       cy.get('.portal-category .portal-tile').last().click();
       // cy.get('#iframe-1').should('be.visible');
       getStore().its('state').its('tabs').its('scrollPosition').should('be.greaterThan', 0);
+    });
+  });
+
+  it('A11y Test', () => {
+    cy.injectAxe();
+    cy.checkA11y('[data-test="header-tabs"]', 
+    {
+      runOnly: {
+        type: 'tag',
+        values: ['wcag21aa'],
+      }
+    },
+    cy.terminalLog, {
+      skipFailures: false
     });
   });
 });
