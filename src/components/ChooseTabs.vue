c06f0de7f9a9a939c1af399daed5700f27598b18
ctopal <topal@univention.de>
2021-09-09 11:23:29 +0200

    [#222] make tabs in modal look like tabs in header

--- src/components/ChooseTabs.vue
+++ src/components/ChooseTabs.vue
@@ -38,32 +38,36 @@ License with the Debian GNU/Linux or Univention distribution in file
         @cancel="cancel"
       >
         <div
-          v-for="(tab, idx) in tabs"
-          :key="idx"
+          v-for="(tab, index) in tabs"
+          :key="index"
           class="choose-tab"
+          :class="{'choose-tab--active': isActiveTab(index)}"
         >
           <div
-            :id="`choose-tab__button--${idx + 1}`"
-            :ref="activeTab === 0 && idx === 0 || activeTab === idx + 1 ? 'currentTab' : ''"
+            :id="`choose-tab__button--${index + 1}`"
+            :ref="isActiveTab(index) ? 'currentTab' : ''"
             class="choose-tab__button"
             tabindex="0"
             :aria-label="ariaLabelChooseTab(tab.tabLabel)"
-            @click.prevent="gotoTab(idx)"
-            @keydown.enter.prevent="gotoTab(idx)"
+            @click.prevent="gotoTab(index)"
+            @keydown.enter.prevent="gotoTab(index)"
           >
-            <img
-              :src="tab.logo"
-              onerror="this.src='./questionMark.svg'"
-              alt=""
-            >
+            <div class="choose-tab__logo-wrapper">
+              <img
+                :src="tab.logo"
+                onerror="this.src='./questionMark.svg'"
+                alt=""
+                class="choose-tab__img"
+              >
+            </div>
             {{ tab.tabLabel }}
           </div>
           <icon-button
             icon="x"
             :active-at="['modal']"
             :aria-label-prop="ariaLabelCloseTab(tab.tabLabel)"
-            :data-test="`chooseTabCloseButton--${idx + 1}`"
-            @click="closeTab(idx)"
+            :data-test="`chooseTabCloseButton--${index + 1}`"
+            @click="closeTab(index)"
           />
         </div>
       </modal-dialog>
@@ -115,47 +119,82 @@ export default defineComponent({
     ariaLabelCloseTab(tabLabel: string): string {
       return `${tabLabel} ${_('Close')}`;
     },
-    closeTab(idx: number) {
-      this.$store.dispatch('tabs/deleteTab', idx + 1);
+    closeTab(index: number) {
+      this.$store.dispatch('tabs/deleteTab', index + 1);
       if (this.tabs.length === 0) {
         this.$store.dispatch('navigation/setActiveButton', '');
       }
     },
-    gotoTab(idx: number) {
-      this.$store.dispatch('tabs/setActiveTab', idx + 1);
+    gotoTab(index: number) {
+      this.$store.dispatch('tabs/setActiveTab', index + 1);
       this.cancel();
     },
     cancel() {
       this.$store.dispatch('navigation/setActiveButton', '');
+    },
+    isActiveTab(index: number): boolean {
+      return (this.activeTab === 0 && index === 0) || this.activeTab === index + 1;
     },
   },
 });
 </script>
 <style lang="stylus">
 .choose-tab
-  margin-top: calc(4 * var(--layout-spacing-unit))
   display: flex
   align-items: center
+  position:relative
+  margin: 2px 0
+  border-radius: var(--border-radius-interactable)
 
   &:first-of-type
     margin-top: 0
 
+  &--active
+    background-color: var(--bgc-apptile-default)
+
+    & ^[0]__logo-wrapper
+      background-color: none
+
   &__button
     display: flex
-    background-color: var(--button-bgc)
     align-items: center
     cursor: pointer
     border: 2px solid transparent
-    border-radius: var(--border-radius-container)
     padding: var(--layout-spacing-unit)
+    padding-left: 0
     width: 100%
 
-    &:focus, &:active, &:hover
-      border-color: var(--color-focus)
+    &:focus
+      outline: 0
 
-    img
-      height: var(--button-size)
-      margin-right: var(--layout-spacing-unit)
+      &:before
+        border-color: var(--color-focus)
+
+    &:before
+      content: ''
+      width: auto
+      height: 100%
+      position: absolute
+      left: 0
+      right: 0
+      border: 2px solid transparent
+
+  &__img
+    width: 80%
+    max-height: 80%
+    vertical-align: middle
+    border: 0
+
+  &__logo-wrapper
+    background-color: var(--bgc-apptile-default)
+    border-radius: var(--border-radius-apptile)
+    height: calc(var(--portal-header-height) * var(--portal-header-icon-scale))
+    width: @height
+    min-width: @height
+    display: flex
+    align-items: center
+    justify-content: center
+    margin: 0 var(--layout-spacing-unit-small) 0 0
 
   .icon-button
     margin-left: var(--layout-spacing-unit)
