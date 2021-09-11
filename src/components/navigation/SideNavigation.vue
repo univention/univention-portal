9de6502859577d97710a7a503dd806a831b2e0c6
ctopal <topal@univention.de>
2021-09-11 08:00:52 +0200

    fix(portal): adjust safe space for iphone
    Issue #321

--- src/components/navigation/SideNavigation.vue
+++ src/components/navigation/SideNavigation.vue
@@ -309,11 +309,11 @@ export default defineComponent({
 $userRow = 6rem
 .portal-sidenavigation
   height: calc(100vh - (var(--portal-header-height) + 0.5rem))
-  height: calc(100vh - (var(--portal-header-height) + 0.5rem + env(safe-area-inset-bottom)))
   display: flex
   flex-direction: column
 
   @media $mqSmartphone
+    height: calc(100vh - (var(--portal-header-height) + 0.5rem + env(safe-area-inset-bottom)))
     overflow-y: auto
 
   &__link
