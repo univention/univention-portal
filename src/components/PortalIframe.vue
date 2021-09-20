ded901ea35f8477f305f72e7b0fcc12aff7cf08d
ctopal <topal@univention.de>
2021-09-20 19:29:28 +0200

    fix(portal): add negative tabindex if modal is open
    Issue #322

--- src/components/PortalIframe.vue
+++ src/components/PortalIframe.vue
@@ -38,7 +38,8 @@ License with the Debian GNU/Linux or Univention distribution in file
       :id="`iframe-${tabId + 1}`"
       ref="iframe"
       :src="link"
-      title="Embedded Page"
+      :title="title"
+      :tabindex="tabindex"
       class="portal-iframe__iframe"
       allow="geolocation; microphone; camera; midi; encrypted-media"
     />
@@ -47,6 +48,7 @@ License with the Debian GNU/Linux or Univention distribution in file
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { mapGetters } from 'vuex';
 
 export default defineComponent({
   name: 'PortalIframe',
@@ -63,12 +65,25 @@ export default defineComponent({
       type: Number,
       required: true,
     },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapGetters({
+      activeButton: 'navigation/getActiveButton',
+    }),
+    tabindex(): number {
+      return this.activeButton === 'copy' ? -1 : 0;
+    },
   },
   mounted() {
     (this.$refs.iframe as HTMLIFrameElement).contentWindow?.focus();
   },
   updated() {
-    if (this.isActive) {
+    if (this.isActive && this.activeButton === '') {
+      console.log('test');
       (this.$refs.iframe as HTMLIFrameElement).contentWindow?.focus();
     }
   },
