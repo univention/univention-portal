4be923f423ce4f7406574759d3c4390ef0a89917
ctopal <topal@univention.de>
2021-09-22 10:03:54 +0200

    [#260] make title appear on two lines ONLY

--- src/components/PortalTile.vue
+++ src/components/PortalTile.vue
@@ -326,8 +326,12 @@ export default defineComponent({
     width: 100%
     overflow: hidden
     text-overflow: ellipsis
-    white-space: nowrap
     text-shadow: 0 0.1rem 0.1rem rgba(0, 0, 0, 0.3)
+    -webkit-box-orient: vertical
+    display: -webkit-box
+    line-clamp: 2
+    -webkit-line-clamp: 2
+    height: calc(2 * var(--font-lineheight-normal))
 
   &__edit-button,
   &__info-button
