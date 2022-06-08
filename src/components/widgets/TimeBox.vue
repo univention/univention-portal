c573d6a9add8394900cc7861a1a9c1c13cd44c58
ctopal <topal@univention.de>
2022-06-08 21:14:57 +0200

    fix(portal): add timebox widget
    Issue #482

---
+++ src/components/widgets/TimeBox.vue
@@ -0,0 +1,58 @@
+<template>
+  <input
+    :id="forAttrOfLabel"
+    ref="input"
+    :name="name"
+    type="time"
+    :value="modelValue"
+    :step="timeBoxStep"
+    :aria-invalid="invalid"
+    :aria-describedby="invalidMessageId || null"
+    data-test="time-box"
+    @input="$emit('update:modelValue', $event.target.value)"
+  >
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { isValid } from '@/jsHelper/forms';
+
+export default defineComponent({
+  name: 'TimeBox',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    invalidMessage: {
+      type: String,
+      default: '',
+    },
+    forAttrOfLabel: {
+      type: String,
+      required: true,
+    },
+    invalidMessageId: {
+      type: String,
+      required: true,
+    },
+    timeBoxStep: {
+      type: Number,
+      default: 60,
+    },
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    invalid(): boolean {
+      return !isValid({
+        type: 'TimeBox',
+        invalidMessage: this.invalidMessage,
+      });
+    },
+  },
+});
+</script>
