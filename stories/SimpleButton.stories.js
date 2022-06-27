0179f925fdeeb9713663f51eaf4b8436cb633cfa
Tan Loc Nguyen <nguyen.extern@univention.de>
2022-06-27 14:16:20 +0200

    fix(portal): storybook
    Issue #0

---
+++ stories/SimpleButton.stories.js
@@ -0,0 +1,33 @@
+import SimpleButton from './SimpleButton.vue';
+
+export default {
+  title: 'Simple Button',
+  component: SimpleButton,
+};
+
+const Template = (args) => ({
+  components: { SimpleButton },
+  setup() {
+    return { args };
+  },
+  template: '<simple-button v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Default Button',
+  variant: 'default'
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  title: 'Light Button',
+  variant: 'light'
+}
+
+export const Dark = Template.bind({});
+Dark.args = {
+  title: 'Dark Button',
+  variant: 'dark'
+}
+
