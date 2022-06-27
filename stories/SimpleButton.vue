bd98bb1968e7f52b6fc9491f2a78f7827372c205
Tan Loc Nguyen <tanloc.nguyen@bcmsolutions.de>
2022-06-27 14:16:20 +0200

    storybook

---
+++ stories/SimpleButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button>
+    aaaaaaaaaaaaaaaaaa
+    {{ title }}
+  </button>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SimpleButton',
+  props: {
+    title: {
+      type: String,
+    },
+  },
+});
+</script>
