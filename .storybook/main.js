bf491e34a5eb0c178c69ff994324134b67eb5aab
ctopal <topal@univention.de>
2022-06-30 23:07:59 +0200

    experiment with component in storybook and add a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,9 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "addon-screen-reader"
   ],
   framework: "@storybook/vue3",
   staticDirs: ['../public'],
