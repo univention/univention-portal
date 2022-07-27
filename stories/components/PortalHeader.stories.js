9b14d17937835f37abfa757dc9fd59b40423920e
ctopal <topal@univention.de>
2022-07-27 15:19:22 +0200

    [#509] add title to portal header story

--- stories/components/PortalHeader.stories.js
+++ stories/components/PortalHeader.stories.js
@@ -1,10 +1,12 @@
 import PortalHeader from '@/components/PortalHeader.vue';
+import { store } from '@/store/';
+
+store.dispatch('portalData/setPortalName', {de_DE: 'Portal', en_US: 'Portal'});
 
 export default {
   title: 'Components/PortalHeader',
   component: PortalHeader,
 };
-
 
 const Template = () => ({
   components: { PortalHeader }, 
