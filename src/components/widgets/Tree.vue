a9b29093a50cc6fac3cbe3ee3d3ce2f655dbd049
Tan Loc Nguyen <nguyen.extern@univention.de>
2022-08-03 23:24:35 +0200

    fix(portal): 484 Tree Widget Create Node Logic, move nested Logic in Node
    Issue #0

--- src/components/widgets/Tree.vue
+++ src/components/widgets/Tree.vue
@@ -1,48 +1,41 @@
 <template>
   <div class="tree">
-    <div
-        :class="['tree-node', `tree-node--level-${getNodeLevel(root)}`, { 'tree-node--selected': root.isSelected }]"
-        @click="onNodeSelect(root)"
-        @dblclick="onNodeExpand(root)"
+    <TransitionGroup
+      name="list"
     >
-      <span
-          :class="['tree-node-arrow-icon', { 'tree-node-arrow-icon--expanded': root.isExpanded }]"
-          @click="onNodeExpand(root)"
-      >
-        <PortalIcon
-            icon="chevron-right"
-        />
-      </span>
-      <span :class="`tree-node-icon tree-node-icon--${root.icon}`" />
-      <span>{{ root.label }}</span>
-    </div>
-    <div
-        v-for="(node, index) in nodes"
-        :key="index"
-        :class="['tree-node', `tree-node--level-${getNodeLevel(node)}`, { 'tree-node--selected': node.isSelected }]"
-        :style="{'--level': getNodeLevel(node)}"
+      <div
+        v-for="node in nodes"
+        :id="node.info.id"
+        :key="node.info.id"
+        :class="['tree-node', { 'tree-node--selected': node.isSelected }]"
+        :style="{ paddingLeft: `${node.level * 1.2}em` }"
         @click="onNodeSelect(node)"
         @dblclick="onNodeExpand(node)"
-    >
-      <span
+      >
+        <span
+          v-if="!isLoading || (isLoading && !node.isSelected)"
           :class="['tree-node-arrow-icon', { 'tree-node-arrow-icon--expanded': node.isExpanded }]"
           @click="onNodeExpand(node)"
-      >
-        <PortalIcon
-            icon="chevron-right"
+        >
+          <PortalIcon icon="chevron-right" />
+        </span>
+        <StandbyCircle
+          v-if="node.isSelected && isLoading"
+          style="width: 16px; height: 16px; padding: 0 var(--layout-spacing-unit)"
         />
-      </span>
-      <span :class="`tree-node-icon tree-node-icon--${node.icon}`" />
-      <span>{{ node.label }}</span>
-    </div>
+        <span :class="`tree-node-icon tree-node-icon--${node.info.icon}`" />
+        <span>{{ node.info.label }}</span>
+      </div>
+    </TransitionGroup>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 import PortalIcon from '@/components/globals/PortalIcon.vue';
+import StandbyCircle from '@/components/StandbyCircle.vue';
 
-interface ListNode {
+interface NodeProps {
   id: string;
   label: string;
   icon: string;
@@ -54,112 +47,184 @@ interface ListNode {
   $isSuperordinate$: boolean;
 }
 
-interface TreeNode extends ListNode {
-  isExpanded: boolean;
-  isSelected: boolean;
-  children?: TreeNode[];
+class Node {
+  public parent: Node | null = null;
+
+  public children: Node[] = [];
+
+  public info: NodeProps;
+
+  public isSelected = false;
+
+  public isExpanded = false;
+
+  constructor(info: NodeProps, parent: Node | null, children: Node[], isSelected = false, isExpanded = false) {
+    this.info = info;
+    this.parent = parent;
+    this.children = children;
+    this.isSelected = isSelected;
+    this.isExpanded = isExpanded;
+  }
+
+  get level(): number {
+    let level = 1;
+    const nodePath = this.info.path.split('/');
+    // check the root node (all nodes always have at least two elements)
+    // but the in the second element of the root node is always empty
+    if (nodePath.length > 1) {
+      if (nodePath[1] === '') return level;
+    }
+    level = nodePath.length;
+    return level;
+  }
+
+  get parentPath(): string | null {
+    // if the node is the root node, it has no parent
+    if (this.level === 1) return null;
+    const nodePath = this.info.path.split('/');
+    nodePath.pop();
+    const parentPath = `${nodePath.join('/')}`;
+    // if this node is the node below the root node, the parent path always has the '/' at the end
+    if (this.level === 2) return `${parentPath}/`;
+    return parentPath;
+  }
+
+  public toggleIsSelected(isSelected?: boolean) {
+    this.isSelected = isSelected ?? !this.isSelected;
+  }
+
+  public toggleIsExpanded(isExpanded?: boolean) {
+    this.isExpanded = isExpanded ?? !this.isExpanded;
+  }
 }
 
 interface Data {
-  root: TreeNode;
-  nodes: TreeNode[];
+  nodes: Node[];
 }
 
 export default defineComponent({
   name: 'Tree',
   components: {
     PortalIcon,
+    StandbyCircle,
   },
   props: {
-    rootNode: {
-      type: Object as PropType<ListNode>,
-      required: true,
-    },
     lists: {
-      type: Array as PropType<ListNode[]>,
+      type: Array as PropType<NodeProps[]>,
       default: () => [],
     },
     onExpand: {
       type: Function as PropType<(id: string) => void>,
-      default: () => () => { },
+      required: true,
     },
     onCollapse: {
-      type: Function as PropType<(id: string) => void>,
-      default: () => () => { },
+      type: Function as PropType<(id: string, childrenId: string[]) => void>,
+      required: true,
     },
-    onAdd: {
-      type: Function as PropType<(id: string) => void>,
-      default: () => () => { },
-    },
-    onEdit: {
-      type: Function as PropType<(id: string) => void>,
-      default: () => () => { },
-    },
-    onRemove: {
-      type: Function as PropType<(id: string) => void>,
-      default: () => () => { },
-    },
-    onSearch: {
-      type: Function as PropType<(id: string) => void>,
-      default: () => () => { },
-    },
-    onMove: {
-      type: Function as PropType<(id: string, parentId: string) => void>,
-      default: () => () => { },
-    },
-    onSubtreeMove: {
-      type: Function as PropType<(id: string, parentId: string) => void>,
-      default: () => () => { },
+    isLoading: {
+      type: Boolean as PropType<boolean>,
+      default: false,
     },
   },
   data(): Data {
     return {
-      root: {
-        ...this.rootNode,
-        isExpanded: true,
-        isSelected: false,
-      },
       nodes: [],
     };
   },
+  watch: {
+    lists: {
+      deep: true,
+      handler(lists: NodeProps[]) {
+        // set new nodes, but keep the old ones with the same id
+        this.nodes = lists.map((newNode) => {
+          const node = this.nodes.find((n) => n.info.id === newNode.id);
+          if (node) {
+            return node;
+          }
+          return new Node(newNode, null, [], false, false);
+        });
+        this.updateNodes();
+      },
+    },
+  },
   mounted() {
-    this.nodes = this.lists.map((node) => ({
-      ...node,
-      isExpanded: false,
-      isSelected: false,
-    }));
-    this.nodes.sort((a, b) => a.label.localeCompare(b.label));
+    this.nodes = this.lists.map((propNode) => {
+      const node = new Node(propNode, null, []);
+      return node;
+    });
+
+    this.updateNodes();
   },
   methods: {
-    onNodeExpand(expandedNode: TreeNode) {
-      if (this.isRootNode(expandedNode)) {
-        this.root.isExpanded = !this.root.isExpanded;
-        this.onNodeSelect(expandedNode);
+    onNodeExpand(expandedNode: Node) {
+      expandedNode.toggleIsExpanded();
+      this.onNodeSelect(expandedNode);
+      if (expandedNode.isExpanded) {
+        this.onExpand(expandedNode.info.id);
         return;
       }
+      // find all children nodes of the expanded node recursively
+      this.onCollapse(expandedNode.info.id, this.findAllChildNodes(expandedNode).map((node) => node.info.id));
+    },
+    onNodeSelect(selectedNode: Node) {
+      this.nodes.forEach((node) => {
+        node.toggleIsSelected(false);
+      });
+      selectedNode.toggleIsSelected(true);
+    },
+    updateNodes() {
+      // find parent and children nodes
       this.nodes = this.nodes.map((node) => {
-        if (node.id === expandedNode.id) {
-          node.isExpanded = !node.isExpanded;
+        const parent = this.nodes.find((n) => n.info.path === node.parentPath);
+        if (parent) {
+          node.parent = parent;
+          parent.children.push(node);
         }
         return node;
       });
-      this.onNodeSelect(expandedNode);
+
+      // group nodes by parent id
+      const nodesByParentId = this.nodes.reduce((acc, node) => {
+        const parentId = node.parent?.info.id ?? null;
+        if (!parentId) return acc;
+        if (!acc[parentId]) {
+          acc[parentId] = [];
+        }
+        acc[parentId].push(node);
+        return acc;
+      }, {} as Record<string, Node[]>);
+
+      // move nodes to after their parent, also sort by level
+      Object.keys(nodesByParentId).forEach((parentId) => {
+        const parentNode = this.nodes.find((n) => n.info.id === parentId);
+        if (!parentNode) return;
+        const children = nodesByParentId[parentId];
+        // sort children by label (alphabetically) descendingly
+        children.sort((a, b) => {
+          if (a.info.label < b.info.label) return 1;
+          if (a.info.label > b.info.label) return -1;
+          return 0;
+        });
+        // move children to after their parent
+        children.forEach((child) => {
+          const index = this.nodes.indexOf(child);
+          if (index === -1) return;
+          this.nodes.splice(index, 1);
+          this.nodes.splice(this.nodes.indexOf(parentNode) + 1, 0, child);
+        });
+      });
     },
-    onNodeSelect(selectedNode: TreeNode) {
-      if (this.isRootNode(selectedNode)) {
-        this.root.isSelected = true;
-        return;
-      }
-      this.nodes = this.nodes.map((node) => ({
-        ...node,
-        isSelected: selectedNode.id === node.id,
-      }));
-    },
-    getNodeLevel(node: TreeNode): number {
-      return node.path.split('/').length - 1;
-    },
-    isRootNode(node: TreeNode): boolean {
-      return node.id === this.rootNode.id;
+    findAllChildNodes(node: Node): Node[] {
+      // find all children nodes of the node recursively
+      const children = node.children;
+      if (children.length === 0) return [];
+      const childrenNodes = children.reduce((currentNodes, child) => {
+        let currentChildNodes = currentNodes;
+        currentChildNodes.push(child);
+        currentChildNodes = currentChildNodes.concat(this.findAllChildNodes(child));
+        return currentChildNodes;
+      }, [] as Node[]);
+      return childrenNodes;
     },
   },
 });
@@ -183,7 +248,7 @@ export default defineComponent({
   text-overflow: ellipsis
   white-space: nowrap
   transition: background-color 0.2s ease-in-out
-  margin-left: calc(var(--level) * 1.2em);
+  cursor: pointer
 
   &:hover
     background-color: var(--bgc-tree-row-hover)
@@ -214,5 +279,12 @@ export default defineComponent({
 
     &--expanded
       transform: rotate(90deg)
+
+.list-enter-active, .list-leave-active
+  transition: all 0.5s ease
+
+.list-enter-from, .list-leave-to
+  opacity: 0
+  transform: translateX(10px)
 
 </style>
