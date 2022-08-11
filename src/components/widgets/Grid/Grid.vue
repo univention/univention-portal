875ec07641e98cf1890f70bb88056a8d733db036
Tan Loc Nguyen <tan-loc.nguyen@intentive.de>
2022-08-11 08:26:54 +0200

    483 ScrollAble Table, new CheckBox Header, Transition, Fix Styling

--- src/components/widgets/Grid/Grid.vue
+++ src/components/widgets/Grid/Grid.vue
@@ -2,11 +2,11 @@
   <div class="grid">
     <Header :is-any-item-selected="isAnyItemSelected" />
     <Table
-      :is-all-items-selected="isAllItemsSelected"
+      :table-header-checkbox="tableHeaderCheckbox"
       :column-label="columnInfo.label"
       :items="gridItems"
       :on-item-selected="onItemSelected"
-      @update:is-all-items-selected="(selected: boolean) => isAllItemsSelected = selected"
+      @update:table-header-checkbox="onTableHeaderCheckboxUpdate"
     />
   </div>
 </template>
@@ -15,10 +15,10 @@
 import { defineComponent, PropType } from 'vue';
 import Header from './Header.vue';
 import Table from './Table.vue';
-import { GridItem, GridItemProps } from './types';
+import { GridItem, GridItemProps, HeaderCheckboxState } from './types';
 
 interface Data {
-  isAllItemsSelected: boolean;
+  tableHeaderCheckbox: HeaderCheckboxState;
   gridItems: GridItem[];
 }
 
@@ -40,20 +40,33 @@ export default defineComponent({
   },
   data(): Data {
     return {
-      isAllItemsSelected: false,
+      tableHeaderCheckbox: false,
       gridItems: [],
     };
   },
   computed: {
-    isAnyItemSelected() {
+    isAnyItemSelected(): boolean {
       return this.gridItems.some((item) => item.selected);
+    },
+    isAllItemsSelected(): boolean {
+      return this.gridItems.every((item) => item.selected);
     },
   },
   watch: {
-    isAllItemsSelected(selected: boolean) {
-      this.gridItems.forEach((item) => {
-        item.selected = selected;
-      });
+    tableHeaderCheckbox(state: HeaderCheckboxState) {
+      console.log(state);
+      if (typeof state === 'boolean') {
+        this.gridItems.forEach((item) => {
+          item.selected = state;
+        });
+      }
+    },
+    isAnyItemSelected(value: boolean) {
+      if (!value) {
+        this.tableHeaderCheckbox = false;
+      } else if (!this.isAllItemsSelected) {
+        this.tableHeaderCheckbox = 'mixed';
+      }
     },
   },
   mounted() {
@@ -65,7 +78,9 @@ export default defineComponent({
   methods: {
     onItemSelected(item: GridItem) {
       item.selected = !item.selected;
-      // this.isAllItemsSelected = this.gridItems.every((item) => item.selected);
+    },
+    onTableHeaderCheckboxUpdate(selected: HeaderCheckboxState) {
+      this.tableHeaderCheckbox = selected;
     },
   },
 });
