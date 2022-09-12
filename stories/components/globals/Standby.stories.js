f9a8e1ed04d24b395d7d2def9bbf5857630f1be6
Tan Loc Nguyen <nguyen.extern@univention.de>
2022-09-12 12:52:46 +0200

    fix(portal): Add Standby Component in Storybook
    Issue #0

---
+++ stories/components/globals/Standby.stories.js
@@ -0,0 +1,17 @@
+import StandbyWrapper from 'components/StandbyWrapper';
+
+export default {
+  title: 'Components/Globals/Standby',
+  components: StandbyWrapper,
+};
+
+// Base Template
+const Template = () => ({
+  components: { StandbyWrapper },
+  setup() {
+    return {};
+  },
+  template: '<standby-wrapper />',
+});
+
+export const Basic = Template.bind({});
