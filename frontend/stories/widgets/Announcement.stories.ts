e51373d48f65dfb8d1ac7a2b87bc337c4847bd00
Tan Loc Nguyen <nguyen.extern@univention.de>
2022-09-27 06:04:51 +0200

    feat: add announcement component
    Issue #0

--- frontend/stories/widgets/Announcement.stories.ts
+++ frontend/stories/widgets/Announcement.stories.ts
@@ -1,6 +1,7 @@
 import { Meta, StoryFn } from '@storybook/vue3';
 
 import Announcement from '../../src/components/widgets/Announcement.vue';
+import PortalIcon from '../../src/components/globals/PortalIcon.vue';
 
 export default {
   title: 'Widgets/Announcement',
@@ -8,18 +9,32 @@ export default {
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['success', 'warning', 'error'],
+      },
+    },
+  },
 } as Meta<typeof Announcement>;
 
 // Base Template
 const Template: StoryFn<typeof Announcement> = (args) => ({
-  components: { Announcement },
+  components: { Announcement, PortalIcon },
   setup() {
     return { args };
   },
-  template: '<div style="width: 500px; padding: 1rem;"><Announcement v-bind="args" /></div>',
+  template: `
+  <div style="max-width: 100vw; padding: 0">
+    <Announcement v-bind="args">
+      <PortalIcon icon="bell" />
+      <p style="margin-left: 4px; font-weight: bold">Announcement!</p>
+    </Announcement>
+  </div>`,
 });
 
 export const Basic = Template.bind({});
 Basic.args = {
-
+  type: 'success',
 };
