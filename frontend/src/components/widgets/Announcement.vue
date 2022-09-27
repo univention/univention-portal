33bc02669e9c30a0a4106c58db41d2edf287b8f0
Tan Loc Nguyen <nguyen.extern@univention.de>
2022-09-27 05:39:41 +0200

    feat: init announcement widget & integrate into FormElement & form.ts
    Issue #0

---
+++ frontend/src/components/widgets/Announcement.vue
@@ -0,0 +1,17 @@
+<template>
+  <div />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Announcement',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
