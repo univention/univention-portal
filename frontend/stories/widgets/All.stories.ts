d4ee6cf697b14b1fa7b59ff69bb91f01c91f09a8
Tan Loc Nguyen <tan.loc.nguyen@hotmail.com>
2022-10-02 05:31:12 +0200

    add some example case for all widgets

--- frontend/stories/widgets/All.stories.ts
+++ frontend/stories/widgets/All.stories.ts
@@ -2,7 +2,6 @@ import { Meta, StoryFn } from '@storybook/vue3';
 
 import MyForm from '@/components/forms/Form.vue';
 import { ref } from 'vue';
-import { validateAll } from '../../src/jsHelper/forms';
 import widgetsJson from '../assets/all/widgets.json';
 
 interface Widget {
@@ -25,11 +24,11 @@ export default {
 const Template: StoryFn<typeof MyForm> = (args) => ({
   components: { MyForm },
   setup() {
-    const widgets: Widget[] = [];
+    const widgets = ref<Widget[]>([]);
     const widgetsModelValue = ref({});
 
-    function isWidgetIsAlreadyAdded(widget: Widget) {
-      return widgets.find((w) => w.type === widget.type);
+    function isWidgetIsAlreadyAdded(widgetType: string) {
+      return widgets.value.find((w) => w.type === widgetType);
     }
 
     // check type is some kind of umc/modules/udm/CertificateUploader
@@ -40,66 +39,64 @@ const Template: StoryFn<typeof MyForm> = (args) => ({
     const ignoreWidgets = ['MultiInput', 'MultiSelect'];
 
     widgetsJson.forEach((widget) => {
-      // ignore widgets
+      // ignore if widget is in the ignore list
       if (ignoreWidgets.includes(widget.type)) return;
 
-      if (!isWidgetIsAlreadyAdded(widget as any)) {
-        if (isTypeIsIncludeSlash(widget as any)) {
-          const widgetType = widget.type.split('/');
-          widget.type = widgetType[widgetType.length - 1];
-        }
-
-        const additionalProps: Record<string, any> = {};
-
-        switch (widget.type) {
-          case 'ComplexInput': {
-            additionalProps.subtypes = [
-              {
-                type: 'TextBox',
-                name: 'text',
-                label: 'TextBox (ComplexInput)',
-              },
-            ];
-            widgetsModelValue.value[widget.type] = [
-              '',
-            ];
-            break;
-          }
-
-          case 'ComboBox': {
-            additionalProps.options = [
-              { id: 'DE', label: 'Germany' },
-              { id: 'US', label: 'United States' },
-            ];
-            widgetsModelValue.value[widget.type] = '';
-            break;
-          }
-
-          case 'MailBox': {
-            additionalProps.domainList = ['intentive.de', 'gmail.com'];
-            widgetsModelValue.value[widget.type] = '';
-            break;
-          }
-
-          case 'ImageUploader': {
-            additionalProps.extraLabel = 'ImageUploader';
-            widgetsModelValue.value[widget.type] = '';
-            break;
-          }
-
-          default:
-            widgetsModelValue.value[widget.type] = '';
-            break;
-        }
-
-        widgets.push({
-          id: widget.type.toLowerCase(),
-          type: widget.type,
-          label: widget.type,
-          name: widget.type,
-          ...additionalProps,
-        });
+      if (isTypeIsIncludeSlash(widget as any)) {
+        const widgetType = widget.type.split('/');
+        widget.type = widgetType[widgetType.length - 1];
       }
+
+      if (isWidgetIsAlreadyAdded(widget.type)) return;
+
+      const additionalProps: Record<string, any> = {};
+
+      switch (widget.type) {
+        case 'ComplexInput': {
+          additionalProps.subtypes = [
+            {
+              type: 'TextBox',
+              name: 'text',
+              label: 'TextBox (ComplexInput)',
+            },
+          ];
+          widgetsModelValue.value[widget.type] = [''];
+          break;
+        }
+
+        case 'ComboBox': {
+          additionalProps.options = [
+            { id: 'DE', label: 'Germany' },
+            { id: 'US', label: 'United States' },
+          ];
+          widgetsModelValue.value[widget.type] = '';
+          break;
+        }
+
+        case 'MailBox': {
+          additionalProps.domainList = ['intentive.de', 'gmail.com'];
+          widgetsModelValue.value[widget.type] = '';
+          break;
+        }
+
+        case 'ImageUploader': {
+          additionalProps.extraLabel = 'ImageUploader';
+          widgetsModelValue.value[widget.type] = '';
+          break;
+        }
+
+        default:
+          widgetsModelValue.value[widget.type] = '';
+          break;
+      }
+
+      widgets.value.push({
+        id: widget.type.toLowerCase(),
+        type: widget.type,
+        label: widget.type,
+        name: widget.type,
+        ...additionalProps,
+      });
     });
 
     return { args, widgets, widgetsModelValue };
@@ -109,96 +106,15 @@ const Template: StoryFn<typeof MyForm> = (args) => ({
     <div>
       <MyForm v-model="widgetsModelValue" :widgets="widgets" />
     </div>`,
-
-  methods: {
-    validate() {
-      validateAll(this.formWidgets, this.formValues);
-    },
-  },
 });
 
 function validator(_widget: any, value: string): string {
   const regex = new RegExp('^[0-9]*$');
   if (!regex.test(value)) {
-    return ('Internal name must not contain anything other than digits, letters or dots, must be at least 2 characters long, and start and end with a digit or letter!');
+    return 'Internal name must not contain anything other than digits, letters or dots, must be at least 2 characters long, and start and end with a digit or letter!';
   }
   return '';
 }
 
 export const Basic = Template.bind({});
-Basic.args = {
-  formWidgets: [
-    {
-      type: 'PasswordBox',
-      name: 'password',
-      label: 'PasswordBox',
-    },
-    {
-      type: 'TextBox',
-      name: 'text',
-      label: 'TextBox',
-      validators: [validator],
-    },
-    {
-      type: 'ComplexInput',
-      name: 'complexInput',
-      label: 'ComplexInput',
-
-      subtypes: [
-        {
-          type: 'TextBox',
-          name: 'text',
-          label: 'TextBox',
-          validators: [validator],
-        },
-        {
-          type: 'DateBox',
-          name: 'date',
-          label: 'date',
-        },
-        {
-          type: 'TimeBox',
-          name: 'time',
-          label: 'time',
-        },
-        {
-          type: 'ComboBox',
-          name: 'timeZone',
-          label: 'timeZone',
-          options: [{
-            id: 'DE',
-            label: 'Germany',
-          }],
-        },
-      ],
-    },
-    {
-      type: 'MultiInput',
-      name: 'multiInput',
-      label: 'MultiInput',
-      extraLabel: 'MultiInput',
-
-      subtypes: [
-        {
-          type: 'DateBox',
-          name: 'date',
-          label: 'date',
-        },
-        {
-          type: 'TimeBox',
-          name: 'time',
-          label: 'time',
-        },
-        {
-          type: 'ComboBox',
-          name: 'timeZone',
-          label: 'timeZone',
-          options: [{
-            id: 'DE',
-            label: 'Germany',
-          }],
-        },
-      ],
-    },
-  ],
-};
+Basic.args = {};
