2c6c178cf544f1762abe52aed0350764cd4bb811
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2022-11-25 17:40:55 +0100

    Await initial screen

--- ansible/vm-vnc-config/configure-vm-via-vnc.py
+++ ansible/vm-vnc-config/configure-vm-via-vnc.py
@@ -1,11 +1,16 @@
-# TODO: environment auto-provided
+#!/usr/bin/env nix-shell
+#!nix-shell -i python3 -p "python3.withPackages(ps: with ps; [ vncdo ])"
 
+import logging
 import os
 
 import vncdotool.api
 
 
 def main():
+    # TODO: setting?
+    logging.basicConfig(level=logging.DEBUG)
+
     # TODO: should come from somewhere
     server = "skurup.knut.univention.de"
     port = "5932"
@@ -13,9 +18,23 @@ def main():
     base_path = get_base_path()
     client = connect_client(server, port)
 
+    # somewhat deterministic mouse, important if watching and accidentally
+    # moving the mouse
+    client.mouseMove(100, 700)
+
     client.expectScreen(os.path.join(base_path, 'screens/await-initial-screen.png'))
 
-    # client.captureScreen(os.path.join(base_path, 'screenshot.png'))
+    raise NotImplementedError("expect worked")
+
+    client.keyPress('tab')
+
+    for k in 'Berlin':
+        client.keyPress(k)
+
+    client.keyPress('tab')
+    client.keyPress('enter')
+
+    client.captureScreen(os.path.join(base_path, 'screenshot.png'))
 
     raise NotImplementedError("implement me!")
 
