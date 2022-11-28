caa02b6e0138eeef8d013f93a2db340a187b9875
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2022-11-28 20:00:45 +0100

    Revert "Log mtime of Cache"
    This reverts commit d6bd636d6f852647d11f0a825b51a83173f51862.

--- python/univention/portal/extensions/cache.py
+++ python/univention/portal/extensions/cache.py
@@ -65,9 +65,7 @@ class Cache(metaclass=Plugin):
 	def get_id(self):
 		try:
 			stat = os.stat(self._cache_file)
-			result = str(stat.st_mtime)
-			get_logger("cache").info("Mtime of cache file {} is {}.".format(
-				self._cache_file, result))
+			return str(stat.st_mtime)
 		except (EnvironmentError):
 			return ""
 
