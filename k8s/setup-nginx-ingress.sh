409d65f4b0ed278b75646902a3cdd0fb94939ad9
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2022-12-03 10:11:50 +0100

    Add an example command to install an ingress controller

---
+++ k8s/setup-nginx-ingress.sh
@@ -0,0 +1,4 @@
+# See https://kubernetes.github.io/ingress-nginx/
+helm upgrade --install ingress-nginx ingress-nginx \
+     --repo https://kubernetes.github.io/ingress-nginx \
+     --namespace ingress-nginx --create-namespace
