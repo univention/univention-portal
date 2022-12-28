455b7236fb279779df4108cea4b86f1803a2aea9
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2022-12-28 17:23:22 +0100

    notifications-api python backend
    Unchanged from Torben's commit

---
+++ notifications-api/app/main.py
@@ -0,0 +1,26 @@
+from app.core.config import get_settings
+from .api import router as api_router
+import logging
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+settings = get_settings()
+description = """
+"""
+app = FastAPI(
+    root_path=settings.root_path,
+    title=settings.project_name,
+    version=settings.api_version,
+    debug=settings.debug,
+    description=description,
+)
+app.include_router(api_router)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    logging.error(f"{request}: {exc_str}")
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
---
+++ notifications-api/app/models/notification_model.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from enum import Enum
+from uuid import UUID
+from typing import Dict, Optional
+from sqlmodel import (
+    JSON,
+    Column,
+    SQLModel,
+    Field,
+)
+
+
+class NotificationType(str, Enum):
+    EVENT = "event"
+    ANNOUNCEMENT = "announcement"
+    ALERT = "alert"
+
+
+class NotificationSeverity(str, Enum):
+    INFO = "info"
+    SUCCESS = "success"
+    WARNING = "warning"
+    ERROR = "error"
+
+
+class NotificationBase(SQLModel):
+    sourceUid: UUID
+    targetUid: UUID
+    title: str
+    details: str
+    severity: NotificationSeverity
+    sticky: Optional[bool]
+    needsConfirmation: Optional[bool]
+    notificationType: NotificationType
+    data: Dict = Field(default={}, sa_column=Column(JSON))
+
+
+class Notification(NotificationBase, table=True):
+    id: UUID = Field(primary_key=True)
+    receiveTime: datetime
+    readTime: Optional[datetime]
+    sseSendTime: Optional[datetime]
+    confirmationTime: Optional[datetime]
+    expireTime: Optional[datetime]
+
+
+class NotificationCreate(NotificationBase):
+    pass
