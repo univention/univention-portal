42a77b17c3172df2a8043e4897c4e9817ba7dd2c
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2022-12-29 14:38:51 +0100

    test(notification-api): Move fixtures into conftest.py
    Consolidates the fixtures into a conftest.py file, so that they can be used by
    all test modules.
    
    Issue: #655

--- notifications-api/tests/test_notification_api.py
+++ notifications-api/tests/test_notification_api.py
@@ -1,66 +1,9 @@
 from datetime import datetime
 import json
-import pytest
-from sqlmodel import Session, create_engine, delete
 from uuid import uuid4
 
-from app.db import get_session
-from app.main import app
-from app.models.notification_model import Notification, NotificationBase
 
-from fastapi.testclient import TestClient
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test2.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-
-def override_get_db() -> Session:
-    with Session(engine) as session:
-        yield session
-
-
-NotificationBase.metadata.create_all(bind=engine)
-app.dependency_overrides[get_session] = override_get_db
-client = TestClient(app)
-db = next(override_get_db())
-
-
-@pytest.fixture()
-def empty_db():
-    statement = delete(Notification)
-    db.exec(statement)
-    db.commit()
-
-
-@pytest.fixture()
-def filled_db(empty_db):
-    db.add(Notification(
-        id=str(uuid4()),
-        details="some details",
-        title="some title",
-        notificationType="event",
-        severity="info",
-        sourceUid=str(uuid4()),
-        targetUid=str(uuid4()),
-        receiveTime=datetime.now()
-    ))
-    db.add(Notification(
-        id=str(uuid4()),
-        details="some details",
-        title="some title",
-        notificationType="announcement",
-        severity="info",
-        sourceUid=str(uuid4()),
-        targetUid=str(uuid4()),
-        receiveTime=datetime.now()
-    ))
-    db.commit()
-
-
-def test_hello():
+def test_hello(client):
     response = client.get('v1')
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, world!"}
@@ -84,7 +27,7 @@ request_data = json.dumps({
 })
 
 
-def test_create_notification(empty_db):
+def test_create_notification(empty_db, client):
     response = client.post('/v1/notifications', request_data)
     assert response.status_code == 201
     response_json = response.json()
@@ -93,13 +36,13 @@ def test_create_notification(empty_db):
     assert response_json['targetUid'] == targetUid
 
 
-def test_get_latest_notifications(filled_db):
+def test_get_latest_notifications(filled_db, client):
     response = client.get('/v1/notifications/latest?page=1&limit=10&type=event')
     assert response.status_code == 200
     assert len(response.json()) == 1
 
 
-def test_mark_notification_read(empty_db):
+def test_mark_notification_read(empty_db, client):
     response = client.post('/v1/notifications', request_data)
     response = client.get('/v1/notifications/latest?page=1&limit=10&type=event')
     id = response.json()[0]['id']
@@ -110,7 +53,7 @@ def test_mark_notification_read(empty_db):
     assert readDateTime > now
 
 
-def test_confirm_notification(empty_db):
+def test_confirm_notification(empty_db, client):
     response = client.post('/v1/notifications', request_data)
     response = client.get('/v1/notifications/latest?page=1&limit=10&type=event')
     id = response.json()[0]['id']
