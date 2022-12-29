e1400d47fe9264bd80239bb09d06db945c396496
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2022-12-29 15:01:59 +0100

    test: clean up left over todo reminder

--- notifications-api/tests/test_notification_service.py
+++ notifications-api/tests/test_notification_service.py
@@ -4,6 +4,5 @@ from app.crud.notification_service import NotificationService
 
 def test_pop_notification_for_sse(filled_db):
     service = NotificationService()
-    # TODO: Why is db to be used instead of filled_db?
     result = service.pop_notifications_for_sse(db=filled_db)
     assert len(result) == 2
