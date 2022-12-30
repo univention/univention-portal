b04d7cbd0123fa75e67384493088cf29b74fad99
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2022-12-30 21:57:50 +0100

    style(notification-api): Remove stale comment in db.py

--- notifications-api/app/db/db.py
+++ notifications-api/app/db/db.py
@@ -4,7 +4,6 @@ from sqlmodel import create_engine, Session
 
 from app.core.config import get_settings
 
-# from api.core.logging import logger
 
 engine = create_engine(
     get_settings().database_url, echo=True, connect_args=dict(check_same_thread=False),
