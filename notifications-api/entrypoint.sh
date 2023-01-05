c8e91a93e18980aae0cf064a6562e9154e056133
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-01-05 21:01:10 +0100

    build: Safeguard auto application of database migrations
    Using the environment variable "APPLY_DATABASE_MIGRATIONS" to prevent that
    people accidentally run into trouble.
    
    Issue: #655

--- notifications-api/entrypoint.sh
+++ notifications-api/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Ensuring database schema is upgraded if needed"
-alembic upgrade head
+if [ "$APPLY_DATABASE_MIGRATIONS" = "true" ]
+then
+    echo "Ensuring database schema is upgraded if needed"
+    alembic upgrade head
+fi
 
 exec "$@"
