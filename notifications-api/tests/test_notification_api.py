05434a6ac34153ecde05e17a05094dfd80d38ae1
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-01-05 13:42:35 +0100

    docs(notifications-api): Group calls into sender and client groups
    Issue: #655

--- notifications-api/tests/test_notification_api.py
+++ notifications-api/tests/test_notification_api.py
@@ -66,6 +66,16 @@ def test_invalidate_one_notification_by_sender(empty_db, client):
     assert response.status_code == "200"
 
 
+@pytest.mark.xfail(reason="Implementation is pending")
+def test_bulk_invalidate_many_notifications_by_sender(empty_db, client):
+    response = client.post('/v1/notifications/', json=request_data)
+    id1 = response.json()['id']
+    response = client.post('/v1/notifications/', json=request_data)
+    id2 = response.json()['id']
+    ids = [id1, id2]
+    response = client.post(f'/v1/notifications/invalidate', json=ids)
+    assert response.status_code == "200"
+
 
 @pytest.mark.asyncio
 async def test_stream_notifications(filled_db, mocker):
