65494b286459637b33e45accf4e63d2574a90bdb
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-01-10 11:36:20 +0100

    feat(frontend): Add connectEventSource to apiclient in notifications
    Issue: #655

--- frontend/src/store/modules/notifications/apiclient.ts
+++ frontend/src/store/modules/notifications/apiclient.ts
@@ -1,8 +1,14 @@
 import { ClientApi, Configuration } from '@/apis/notifications';
 
-const notificationsApiUrl = process.env.VUE_APP_NOTIFICATIONS_API_URL || './notifications-api';
+export const notificationsApiUrl = process.env.VUE_APP_NOTIFICATIONS_API_URL || './notifications-api';
 const notificationsApi = new ClientApi(new Configuration({
   basePath: notificationsApiUrl,
 }));
+
+export const connectEventSource = function (): EventSource {
+  const streamUrl = `${notificationsApiUrl}/v1/notifications/stream`;
+  const eventSource = new EventSource(streamUrl);
+  return eventSource;
+};
 
 export default notificationsApi;
