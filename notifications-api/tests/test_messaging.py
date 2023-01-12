e68580379e34944e046a72ba6ab365aceb4feb0c
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-01-12 14:45:23 +0100

    test(notifications-api): Cover _ensure_socket

--- notifications-api/tests/test_messaging.py
+++ notifications-api/tests/test_messaging.py
@@ -15,3 +15,28 @@ async def test_publish_notification_sends_message_via_socket(mocker):
     await messaging.publish_notification(stub_topic, stub_data)
     expected_message = (stub_topic.encode(), stub_data.encode())
     mock_socket.send_multipart.assert_called_once_with(expected_message)
+
+
+@pytest.mark.asyncio
+async def test_ensure_socket_creates_socket(mocker):
+    mock_socket = mock.MagicMock()
+    mock_socket.recv = mock.AsyncMock()
+    mock_context = mock.MagicMock()
+    mock_context.socket.return_value = mock_socket
+
+    mocker.patch.object(messaging, 'context', mock_context)
+
+    result = await messaging._ensure_socket()
+
+    assert result == mock_socket
+
+
+@pytest.mark.asyncio
+async def test_ensure_socket_creates_no_socket():
+    mock_socket = mock.MagicMock()
+    mock_socket.recv = mock.AsyncMock()
+
+    messaging._thread_local.socket = mock_socket
+    result = await messaging._ensure_socket()
+
+    assert result == mock_socket
