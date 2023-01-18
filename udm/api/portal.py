4be14b7ad4d266cfc1c9ea31dbb5c684552d6757
Florian Best <best@univention.de>
2023-01-19 00:17:31 +0100

    style: add missing trailing commatas
    ruff --fix --select COM812 $(python_files)
    
    ignore packaging/univention-l10n/univention/l10n/umc.py as it's not
    Python 2 compatible
    
    Bug #31771

--- udm/api/portal.py
+++ udm/api/portal.py
@@ -36,7 +36,7 @@ Module and object specific for "portals/portal" UDM module.
 from __future__ import absolute_import, unicode_literals
 from ..encoders import (
     dn_list_property_encoder_for, Base64BinaryPropertyEncoder, StringCaseInsensitiveResultUpperBooleanPropertyEncoder,
-    ListOfListOflTextToDictPropertyEncoder, DatePropertyEncoder, BaseEncoder
+    ListOfListOflTextToDictPropertyEncoder, DatePropertyEncoder, BaseEncoder,
 )
 from .generic import GenericModule, GenericObject, GenericObjectProperties
 
