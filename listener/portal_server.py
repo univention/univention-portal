77df3c53437bf2fc380669fb30be7d6bfa127d16
Florian Best <best@univention.de>
2023-01-23 16:49:38 +0100

    style(python): apply isort
    isort --py 37 $(python_files) $(find -name '*.pyi')
    Bug #31771

--- listener/portal_server.py
+++ listener/portal_server.py
@@ -40,7 +40,7 @@ import univention.debug as ud
 
 import listener
 
-name = 'portal_server'
+
 description = 'Tell portal server to refresh when something important changed'
 filter = '(|(univentionObjectType=portals/portal)(univentionObjectType=portals/category)(univentionObjectType=portals/entry)(univentionObjectType=portals/folder))'
 
