a243bbf77e2f8e81953f0f54313ef2ae6f2de68a
Sebastian König-Festl <sebastian.koenig.extern@univention.de>
2023-01-31 11:55:26 +0000

    fix(portal): revert fix storybook issues with password box
    This reverts commit f9363e149d68fb64dc93195cc7b18c064de34de8.
    
    Issue: #4

--- frontend/src/views/selfservice/NewPassword.vue
+++ frontend/src/views/selfservice/NewPassword.vue
@@ -1,9 +1,37 @@
+<!--
+  Copyright 2021-2022 Univention GmbH
+
+  https://www.univention.de/
+
+  All rights reserved.
+
+  The source code of this program is made available
+  under the terms of the GNU Affero General Public License version 3
+  (GNU AGPL V3) as published by the Free Software Foundation.
+
+  Binary versions of this program provided by Univention to you as
+  well as other copyrighted, protected or trademarked materials like
+  Logos, graphics, fonts, specific documentations and configurations,
+  cryptographic keys etc. are subject to a license agreement between
+  you and Univention and not subject to the GNU AGPL V3.
+
+  In the case you use this program under the terms of the GNU AGPL V3,
+  the program is provided in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public
+  License with the Debian GNU/Linux or Univention distribution in file
+  /usr/share/common-licenses/AGPL-3; if not, see
+  <https://www.gnu.org/licenses/>.
+-->
 <template>
   <site
     :title="TITLE"
     :subtitle="SUBTITLE"
   >
-    <my-form_ctx.invalidMessageId
+    <my-form
       ref="form"
       v-model="formValues"
       :widgets="formWidgetsWithTabindex"
@@ -18,7 +46,7 @@
           {{ SUBMIT_LABEL }}
         </button>
       </footer>
-    </my-form_ctx.invalidMessageId>
+    </my-form>
     <error-dialog
       ref="errorDialog"
     />
@@ -32,7 +60,7 @@ import { umcCommand } from '@/jsHelper/umc';
 import _ from '@/jsHelper/translate';
 import Site from '@/views/selfservice/Site.vue';
 import MyForm from '@/components/forms/Form.vue';
-import { validateAll, isEmpty, WidgetDefinition } from '@/jsHelper/forms';
+import { validateAll, WidgetDefinition, validateNewPassword } from '@/jsHelper/forms';
 import ErrorDialog from '@/views/selfservice/ErrorDialog.vue';
 import activity from '@/jsHelper/activity';
 import { mapGetters } from 'vuex';
@@ -40,8 +68,7 @@ import { mapGetters } from 'vuex';
 interface FormData {
   username: string,
   token: string,
-  newPassword: string,
-  newPassword2: string,
+  setNewPassword: Record<string, string>,
 }
 
 interface Data {
@@ -82,30 +109,21 @@ export default defineComponent({
       invalidMessage: '',
       required: true,
     }, {
-      type: 'PasswordBox',
-      name: 'newPassword',
+      type: 'NewPasswordBox',
+      name: 'setNewPassword',
       label: _('New password'),
-      invalidMessage: '',
+      canShowPassword: true,
       required: true,
-    }, {
-      type: 'PasswordBox',
-      name: 'newPassword2',
-      label: _('New password (retype)'),
-      validators: [(widget, value) => (
-        isEmpty(widget, value) ? _('Please confirm your new password') : ''
-      ), (widget, value, widgets, values) => {
-        if (values.newPassword !== value) {
-          return _('The new passwords do not match');
-        }
-        return '';
-      }],
+      validators: [validateNewPassword],
     }];
     return {
       formValues: {
         username: this.queryParamUsername,
         token: this.queryParamToken,
-        newPassword: '',
-        newPassword2: '',
+        setNewPassword: {
+          newPassword: '',
+          retypePassword: '',
+        },
       },
       formWidgets,
     };
@@ -177,7 +195,7 @@ export default defineComponent({
       const params = {
         username: this.formValues.username,
         token: this.formValues.token,
-        password: this.formValues.newPassword,
+        password: this.formValues.setNewPassword.newPassword,
       };
       this.$store.dispatch('activateLoadingState');
       umcCommand('passwordreset/set_password', params)
