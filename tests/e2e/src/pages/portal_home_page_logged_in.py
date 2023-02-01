b5d54e69b6e8ceecbd2255c5b5be208913fa9e1c
Dibya Chakravorty <dibya.chakravorty.extern@univention.de>
2023-02-01 15:24:22 +0100

    test: add login and home pages

---
+++ tests/e2e/src/pages/portal_home_page_logged_in.py
@@ -0,0 +1,10 @@
+from pages.portal_page import PortalPage
+
+
+class PortalHomePageLoggedIn(PortalPage):
+    """
+    This represents the logged in state of the portal's home page.
+    """
+    def __init__(self, page):
+        super().__init__(page)
+        self.umc_heading = page.get_by_text("Univention Management Console")
---
+++ tests/e2e/src/pages/portal_page.py
@@ -0,0 +1,36 @@
+from pages.base.base_page import BasePage
+from pages.header import Header
+from pages.notification_drawer import NotificationDrawer
+from pages.notification_popup import PopupNotificationContainer
+
+
+class PortalPage(BasePage):
+    """
+    All portal pages should be derived from this class.
+    Access to shared parts (i.e. parts that all pages have) are provided using
+    instance variables. For example, to get the navigation header, you can do
+    the following.
+
+    ```
+    class MyPage(PortalPage):
+        ...
+
+    my_page = MyPage(page)
+    header = my_page.header  # Get the header
+    # Now, do something with the header
+    header.reveal_notification_drawer()
+    ```
+    """
+    def __init__(self, page):
+        super().__init__(page)
+        self.header = Header(self.page,
+                             self.page.locator("#portal-header"),
+                             self
+                             )
+        self.notification_drawer = NotificationDrawer(self.page,
+                                                      self.page.locator("#notifications-all"),
+                                                      self
+                                                      )
+        self.popup_notification_container = PopupNotificationContainer(
+            self.page, self.page.locator("#notifications-visible"), self
+        )
