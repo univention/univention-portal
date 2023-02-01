301b6aff26415b88d354c32645d312412a963c46
Dibya Chakravorty <dibya.chakravorty.extern@univention.de>
2023-02-01 15:22:55 +0100

    test: add portal specific exception

---
+++ tests/e2e/src/pages/exceptions.py
@@ -0,0 +1,6 @@
+class PortalError(Exception):
+    """
+    Should be raised by Page Object methods if an expectation from the portal
+    is violated and playwright's expect() cannot be used for some reason.
+    """
+    pass
