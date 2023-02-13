f67963752bc4deb2705cd7eb42e3b70b4ce199c0
Florian Best <best@univention.de>
2023-02-13 18:43:08 +0100

    style: fix PIE790

--- tests/e2e/src/pages/exceptions.py
+++ tests/e2e/src/pages/exceptions.py
@@ -3,4 +3,3 @@ class PortalError(Exception):
     Should be raised by Page Object methods if an expectation from the portal
     is violated and playwright's expect() cannot be used for some reason.
     """
-    pass
