univention/univention-corporate-server

style: convert tabs to spaces in Python files
autopep8 -i --select E1,W191 -j0 --ignore-local-config $(python_files) $(find -name '*.pyi')

$ autopep8 --version
autopep8 1.7.1 (pycodestyle: 2.9.1)

(actually used fixers: E101,E123,E124,E126,E127,E128,E131,W191)

Bug #31771

management/univention-portal/unittests/test_config.py
@@ -40,45 +40,45 @@ import pytest
 
 @pytest.fixture
 def mocked_portal_config(portal_config, get_file_path):
-	reload(portal_config)
-	portal_config._CONF = get_file_path("config*.json")
-	return portal_config
+    reload(portal_config)
+    portal_config._CONF = get_file_path("config*.json")
+    return portal_config
 
 
 def test_load_config_success(mocked_portal_config):
-	# Set up
-	mocked_portal_config._DB = {"old": "value"}
-	expected_config = {"port": 8090, "fqdn": "dataport.ucs", "url": "http://127.0.0.1:8090", "test": True}
-	# Execute
-	assert mocked_portal_config.load.never_loaded is True
-	mocked_portal_config.load()
-	assert mocked_portal_config.load.never_loaded is False
-	assert mocked_portal_config._DB == expected_config
+    # Set up
+    mocked_portal_config._DB = {"old": "value"}
+    expected_config = {"port": 8090, "fqdn": "dataport.ucs", "url": "http://127.0.0.1:8090", "test": True}
+    # Execute
+    assert mocked_portal_config.load.never_loaded is True
+    mocked_portal_config.load()
+    assert mocked_portal_config.load.never_loaded is False
+    assert mocked_portal_config._DB == expected_config
 
 
 def test_load_config_error(mocker, mocked_portal_config):
-	# Set up
-	mocked_portal_config._DB = {"old": "value"}
-	mocker.patch.object(mocked_portal_config, "open", side_effect=EnvironmentError)
-	# Execute
-	assert mocked_portal_config.load.never_loaded is True
-	mocked_portal_config.load()
-	assert mocked_portal_config._DB == {}
-	assert mocked_portal_config.load.never_loaded is True
-	assert mocked_portal_config._DB == {}
+    # Set up
+    mocked_portal_config._DB = {"old": "value"}
+    mocker.patch.object(mocked_portal_config, "open", side_effect=EnvironmentError)
+    # Execute
+    assert mocked_portal_config.load.never_loaded is True
+    mocked_portal_config.load()
+    assert mocked_portal_config._DB == {}
+    assert mocked_portal_config.load.never_loaded is True
+    assert mocked_portal_config._DB == {}
 
 
 def test_fetch_key(mocker, mocked_portal_config):
-	# Set up
-	def config_loaded():
-		mocked_portal_config.load.never_loaded = False
+    # Set up
+    def config_loaded():
+        mocked_portal_config.load.never_loaded = False
 
-	load_mock = mocker.patch.object(mocked_portal_config, "load", side_effect=config_loaded)
-	mocked_portal_config._DB = {"port": 443, "fqdn": "dataport.ucs"}
-	# Execute
-	assert mocked_portal_config.fetch("port") == 443
-	assert mocked_portal_config.load.never_loaded is False
-	assert mocked_portal_config.fetch("fqdn") == "dataport.ucs"
-	with pytest.raises(KeyError):
-		mocked_portal_config.fetch("no_key")
-	load_mock.assert_called_once()
+    load_mock = mocker.patch.object(mocked_portal_config, "load", side_effect=config_loaded)
+    mocked_portal_config._DB = {"port": 443, "fqdn": "dataport.ucs"}
+    # Execute
+    assert mocked_portal_config.fetch("port") == 443
+    assert mocked_portal_config.load.never_loaded is False
+    assert mocked_portal_config.fetch("fqdn") == "dataport.ucs"
+    with pytest.raises(KeyError):
+        mocked_portal_config.fetch("no_key")
+    load_mock.assert_called_once()

univention/univention-portal

unittests/test_config.py
@@ -40,47 +40,47 @@ import pytest
 
 @pytest.fixture
 def mocked_portal_config(get_file_path):
-	from univention.portal import config
+    from univention.portal import config
 
-	reload(config)
-	config._CONF = get_file_path("config*.json")
-	return config
+    reload(config)
+    config._CONF = get_file_path("config*.json")
+    return config
 
 
 def test_load_config_success(mocked_portal_config):
-	# Set up
-	mocked_portal_config._DB = {"old": "value"}
-	expected_config = {"port": 8090, "fqdn": "dataport.ucs", "url": "http://127.0.0.1:8090", "test": True}
-	# Execute
-	assert mocked_portal_config.load.never_loaded is True
-	mocked_portal_config.load()
-	assert mocked_portal_config.load.never_loaded is False
-	assert mocked_portal_config._DB == expected_config
+    # Set up
+    mocked_portal_config._DB = {"old": "value"}
+    expected_config = {"port": 8090, "fqdn": "dataport.ucs", "url": "http://127.0.0.1:8090", "test": True}
+    # Execute
+    assert mocked_portal_config.load.never_loaded is True
+    mocked_portal_config.load()
+    assert mocked_portal_config.load.never_loaded is False
+    assert mocked_portal_config._DB == expected_config
 
 
 def test_load_config_error(mocker, mocked_portal_config):
-	# Set up
-	mocked_portal_config._DB = {"old": "value"}
-	mocker.patch.object(mocked_portal_config, "open", side_effect=EnvironmentError)
-	# Execute
-	assert mocked_portal_config.load.never_loaded is True
-	mocked_portal_config.load()
-	assert mocked_portal_config._DB == {}
-	assert mocked_portal_config.load.never_loaded is True
-	assert mocked_portal_config._DB == {}
+    # Set up
+    mocked_portal_config._DB = {"old": "value"}
+    mocker.patch.object(mocked_portal_config, "open", side_effect=EnvironmentError)
+    # Execute
+    assert mocked_portal_config.load.never_loaded is True
+    mocked_portal_config.load()
+    assert mocked_portal_config._DB == {}
+    assert mocked_portal_config.load.never_loaded is True
+    assert mocked_portal_config._DB == {}
 
 
 def test_fetch_key(mocker, mocked_portal_config):
-	# Set up
-	def config_loaded():
-		mocked_portal_config.load.never_loaded = False
+    # Set up
+    def config_loaded():
+        mocked_portal_config.load.never_loaded = False
 
-	load_mock = mocker.patch.object(mocked_portal_config, "load", side_effect=config_loaded)
-	mocked_portal_config._DB = {"port": 443, "fqdn": "dataport.ucs"}
-	# Execute
-	assert mocked_portal_config.fetch("port") == 443
-	assert mocked_portal_config.load.never_loaded is False
-	assert mocked_portal_config.fetch("fqdn") == "dataport.ucs"
-	with pytest.raises(KeyError):
-		mocked_portal_config.fetch("no_key")
-	load_mock.assert_called_once()
+    load_mock = mocker.patch.object(mocked_portal_config, "load", side_effect=config_loaded)
+    mocked_portal_config._DB = {"port": 443, "fqdn": "dataport.ucs"}
+    # Execute
+    assert mocked_portal_config.fetch("port") == 443
+    assert mocked_portal_config.load.never_loaded is False
+    assert mocked_portal_config.fetch("fqdn") == "dataport.ucs"
+    with pytest.raises(KeyError):
+        mocked_portal_config.fetch("no_key")
+    load_mock.assert_called_once()