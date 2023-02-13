cfdc46a06e37f3c1d5824e4ff424d2570e71076a
Florian Best <best@univention.de>
2023-02-13 18:53:19 +0100

    style: fix D200

--- tests/e2e/src/pages/portal_home_page_logged_in.py
+++ tests/e2e/src/pages/portal_home_page_logged_in.py
@@ -2,9 +2,7 @@ from pages.portal_page import PortalPage
 
 
 class PortalHomePageLoggedIn(PortalPage):
-    """
-    This represents the logged in state of the portal's home page.
-    """
+    """This represents the logged in state of the portal's home page."""
     def __init__(self, page):
         super().__init__(page)
         self.umc_heading = page.get_by_text("Univention Management Console")
