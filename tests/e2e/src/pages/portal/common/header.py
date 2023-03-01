49e10b2e211325290b0479b4c6e98aadc2f6aee5
Dibya Chakravorty <dibya.chakravorty.extern@univention.de>
2023-03-01 15:58:26 +0100

    refactor: Add right side menu, refactor notification related classes into one file

--- tests/e2e/src/pages/portal/common/header.py
+++ tests/e2e/src/pages/portal/common/header.py
@@ -1,25 +1,20 @@
 import re
 
-from playwright.sync_api import expect
-
-from pages.base.base_page_part import BasePagePart
+from pages.base import BasePagePart
 
 
 class Header(BasePagePart):
     """This represents the portal's top navigation header bar."""
 
-    def __init__(self, page, part, parent):
-        super().__init__(page, part, parent)
-        self.bell_icon = self.part.get_by_role("button",
-                                               name=re.compile("^Notifications"),
-                                               )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bell_icon = self.get_by_role("button",
+                                          name=re.compile("^Notifications"),
+                                          )
+        self.hamburger_icon = self.get_by_role("button", name="Menu")
 
-    def reveal_notification_drawer(self):
-        if self.parent.notification_drawer.is_hidden():
-            self.bell_icon.click()
-            expect(self.parent.notification_drawer.part).to_be_visible()
+    def click_bell_icon(self):
+        self.bell_icon.click()
 
-    def hide_notification_drawer(self):
-        if self.parent.notification_drawer.is_visible():
-            self.bell_icon.click()
-            expect(self.parent.notification_drawer.part).to_be_hidden()
+    def click_hamburger_icon(self):
+        self.hamburger_icon.click()
