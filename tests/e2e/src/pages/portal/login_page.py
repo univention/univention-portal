597cd3c413ddba5cf94e9f1b3b41969136731f45
Dibya Chakravorty <dibya.chakravorty.extern@univention.de>
2023-03-01 16:24:08 +0100

    refactor: change login page's navigation logic

--- tests/e2e/src/pages/portal/login_page.py
+++ tests/e2e/src/pages/portal/login_page.py
@@ -1,12 +1,26 @@
+from pages.base import expect
 from pages.portal.common.portal_page import PortalPage
+from pages.portal.home_page.logged_out import HomePageLoggedOut
 
 
 class LoginPage(PortalPage):
-    def __init__(self, page):
-        super().__init__(page)
-        self.username_input = page.get_by_label("Username")
-        self.password_input = page.get_by_label("Password", exact=True)
-        self.login_button = page.get_by_role("button", name="Login")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.username_input = self.page.get_by_label("Username")
+        self.password_input = self.page.get_by_label("Password", exact=True)
+        self.login_button = self.page.get_by_role("button", name="Login")
+
+    def navigate(self):
+        home_page = HomePageLoggedOut(self.page)
+        home_page.navigate()
+        home_page.check_its_there()
+        self.reveal_right_side_menu()
+        expect(self.right_side_menu.login_button).to_be_visible()
+        expect(self.right_side_menu.logout_button).to_be_hidden()
+        self.right_side_menu.click_login_button()
+
+    def check_its_there(self):
+        expect(self.username_input).to_be_visible()
 
     def fill_username(self, username):
         self.username_input.fill(username)
