98f03331af6009bc1f78984c93586ba1c3f11183
Sebastian König-Festl <sebastian.koenig.extern@univention.de>
2023-03-07 12:58:30 +0000

    test: add storybook for testing new password box
    Issue: #4

---
+++ frontend/stories/widgets/NewPasswordBox.stories.ts
@@ -0,0 +1,36 @@
+import { Meta, StoryFn } from '@storybook/vue3';
+
+import NewPasswordBox from '../../src/components/widgets/NewPasswordBox.vue';
+
+export default {
+  title: 'Widgets/NewPasswordBox',
+  components: NewPasswordBox,
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<typeof NewPasswordBox>;
+
+// Base Template
+const Template: StoryFn<typeof NewPasswordBox> = (args) => ({
+  components: { NewPasswordBox },
+  setup() {
+    return { args };
+  },
+  template: `
+  <div>
+    <new-password-box v-bind='args' />
+  </div>`
+});
+
+export const Basic = Template.bind({});
+Basic.args = {
+  name: "my name",
+  modelValue: {"": ""},
+  invalidMessage: {"": ""},
+  forAttrOfLabel: "forAttrOfLabel",
+  invalidMessageId: "invalidMessageId",
+  disabled: false,
+  tabindex: 0,
+  required: true,
+  canShowPassword: true,
+};
