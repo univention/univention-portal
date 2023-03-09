af16e0a985629a1ec0c93cfb1631e54b3abe9f44
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-03-09 15:00:29 +0100

    refactor(portal-frontend): Use "icon_url" instead of "logo_name"
    Issue: #598

--- frontend/src/jsHelper/portalCategories.ts
+++ frontend/src/jsHelper/portalCategories.ts
@@ -72,7 +72,7 @@ function makeEntry(
       linkTarget: entry.linkTarget === 'useportaldefault' ? defaultLinkTarget : entry.linkTarget,
       target: entry.target,
       originalLinkTarget: entry.linkTarget,
-      pathToLogo: entry.logo_name || './media/questionmark.svg',
+      pathToLogo: entry.icon_url || './media/questionmark.svg',
       key: {
         de_DE: 'de_DE',
         en_US: 'en_US',
