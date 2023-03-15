7492d17123e83573463f78d2852904f10f261519
Dibya Chakravorty <dibya.chakravorty.extern@univention.de>
2023-03-15 10:12:23 +0100

    fix: Change base class to concrete class representing a page

--- tests/e2e/tests/test_i18n.py
+++ tests/e2e/tests/test_i18n.py
@@ -1,10 +1,10 @@
 from pages.base import expect
-from pages.portal.home_page.logged_in import HomePage
+from pages.portal.home_page.logged_out import HomePageLoggedOut
 
 
 def test_user_can_switch_language_to_german(navigate_to_home_page_logged_out):
     page = navigate_to_home_page_logged_out
-    home_page = HomePage(page)
+    home_page = HomePageLoggedOut(page)
     home_page.switch_language("Deutsch")
 
     assert home_page.get_language() == "de"
@@ -16,7 +16,7 @@ def test_user_can_switch_language_to_german(navigate_to_home_page_logged_out):
 
 def test_user_can_switch_language_to_english(navigate_to_home_page_logged_out):
     page = navigate_to_home_page_logged_out
-    home_page = HomePage(page)
+    home_page = HomePageLoggedOut(page)
     home_page.switch_language("Deutsch")
     home_page.switch_language("English")
 
