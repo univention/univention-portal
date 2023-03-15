989f2e37a4a450e6cdc87f0f49bd8e26537b3b31
Dibya Chakravorty <dibya.chakravorty.extern@univention.de>
2023-03-15 11:50:04 +0100

    fix: Move navigation to the test so that negative case (with non-admin user) leads to FAILURE instead of ERROR

--- tests/e2e/tests/test_users_page.py
+++ tests/e2e/tests/test_users_page.py
@@ -1,10 +1,16 @@
 from pages.base import expect
+from pages.portal.home_page.logged_in import HomePageLoggedIn
 from pages.portal.users.users_page import UsersPage
 
 
-def test_user_list(navigate_to_users_page):
-    page, tab_admin = navigate_to_users_page
-    users_page = UsersPage(tab_admin)
+def test_admin_user_can_view_users_page(navigate_to_home_page_logged_in):
+    """This test should be run using an admin user. Otherwise, it will fail."""
+    page = navigate_to_home_page_logged_in
+    home_page_logged_in = HomePageLoggedIn(page)
+    # TODO: We don't yet have a concept for popups in our POM.
+    with page.expect_popup() as tab_admin:
+        home_page_logged_in.click_users_tile()
+    users_page = UsersPage(tab_admin.value)
     # TODO: The user list takes unnaturally long to appear. We are using a locator timeout
     # to handle that. Replace this with an increased global timeout as soon as we figure out how.
     expect(users_page.add_user_button).to_be_visible(timeout=10000)
