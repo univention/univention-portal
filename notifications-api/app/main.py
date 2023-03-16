32dd829933a47f27c94b02fe985d3293adfbd29d
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-03-16 10:56:04 +0100

    docs(notifications-api): Update introduction section
    Moved the introduction section from the XWiki page into the code and adapted it
    to correctly reflect the current state of the API's implementation.

--- notifications-api/app/main.py
+++ notifications-api/app/main.py
@@ -13,19 +13,56 @@ from app.core.config import settings
 
 
 description = """
-The Notifications API is intended to be used so that
-applications can send notifications so that they are visible within the
-Univention Portal.
+
+
+## Overview
+
+The Notifications API is part of the Univention Portal (Portal) and allows other
+services to integrate their user facing notifications with the Portal so that
+the user has a holistic view when using the Portal.
+
+
+### Intended usage scenario
+
+- The delivery of notifications from other services to the API goes backend to
+  backend – the API then delivers the notifications to the Portal.
+
+- The notification API service is always available for services to publish
+  notifications or to updated their published notifications.
+
+- Notifications are pushed to the user if the user is online in the Portal.
+
+- If the user is offline the API will cache the notifications and deliver them
+  as soon as the user is again online in the Portal.
+
+
+### API structure
 
 The API endpoints can be grouped as follows:
 
 - *Notification Sender API* -- This subset of endpoints is intended to be used
-  by applications who want to send a notification for a given user.
+  by services who want to send a notification for a given user.
 
-- *Notification Client API* -- This subset shall fulfill the needs of the
+- *Notification Receiver API* -- This subset shall fulfill the needs of the
   Univention Portal, so that it can show the notifications to the user.
 
-Note: The groups are not necessarily disjoint.
+The groups may slightly overlap where appropriate.
+
+
+### API stability and backwards compatibility
+
+A decision about the commitments regarding stability and backwards
+compatibility has not yet been made.
+
+As a rule of thumb, the Sender part of the API is expected to have such a
+commitment once it stabilizes and reaches closer to the point of it's first
+release.
+
+A scenario to have also other clients besides the Portal has not been taken
+into account yet. Currently the Receiver section of the API is considered
+private to the Univention Portal and is not accompanied by any stability
+commitments.
+
 """
 
 
