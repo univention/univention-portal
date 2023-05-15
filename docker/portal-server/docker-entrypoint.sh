3edf9708d45fba14c9951a793e064265ffc62cf2
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-05-15 11:41:48 +0200

    build(listener): Provide needed configuration options

--- docker/portal-server/docker-entrypoint.sh
+++ docker/portal-server/docker-entrypoint.sh
@@ -38,11 +38,19 @@ IFS='' read -r -d '' JQ_TEMPLATE <<"EOF" || true
   "admin_groups": [
     $admin_group
   ],
+  "assets_root": "/usr/share/univention-portal",
   "auth_mode": $auth_mode,
-  "editable": $editable,
-  "port": $port,
   "auth_secret": $auth_secret,
+  "default_domain_dn": $default_domain_dn,
+  "editable": $editable,
+  "hostdn": $hostdn,
+  "ldap_base": $ldap_base,
+  "ldap_uri": $ldap_uri,
+  "port": $port,
   "ucs_internal_url": $ucs_internal_url,
+  "udm_api_url": $udm_api_url,
+  "udm_api_username": $udm_api_username,
+  "udm_api_password_file": $udm_api_password_file,
   "umc_get_url": $umc_get_url,
   "umc_session_url": $umc_session_url,
   "umc_check_icons": $umc_check_icons
@@ -54,10 +62,17 @@ echo "Generating ${JSON_PATH}"
 jq -n \
   --arg admin_group "${PORTAL_SERVER_ADMIN_GROUP}" \
   --arg auth_mode "${PORTAL_SERVER_AUTH_MODE}" \
-  --argjson editable "${PORTAL_SERVER_EDITABLE}" \
-  --arg port "${PORTAL_SERVER_PORT}" \
   --arg auth_secret "${PORTAL_SERVER_AUTH_SECRET}" \
+  --arg default_domain_dn "${PORTAL_DEFAULT_DN:-}" \
+  --argjson editable "${PORTAL_SERVER_EDITABLE}" \
+  --arg hostdn "${LDAP_HOST_DN:-}" \
+  --arg ldap_base "${LDAP_BASE_DN:-}" \
+  --arg ldap_uri "ldap://${LDAP_HOST:-}:${LDAP_PORT}" \
+  --arg port "${PORTAL_SERVER_PORT}" \
   --arg ucs_internal_url "${PORTAL_SERVER_UCS_INTERNAL_URL}" \
+  --arg udm_api_url "${PORTAL_UDM_API_URL:-}" \
+  --arg udm_api_username "${PORTAL_UDM_API_USERNAME:-}" \
+  --arg udm_api_password_file "${PORTAL_UDM_API_PASSWORD_FILE:-}" \
   --arg umc_get_url "${PORTAL_SERVER_UMC_GET_URL}" \
   --arg umc_session_url "${PORTAL_SERVER_UMC_SESSION_URL}" \
   --argjson umc_check_icons "${PORTAL_SERVER_UMC_CHECK_ICONS}" \
