d2e9cd3a8557e2cd4aa7f4bca64a53cae08131ae
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-05-31 14:47:31 +0200

    feat(listener): Support env variable MACHINE_SECRET_FILE in entrypoint

--- docker/portal-listener/portal-listener-entrypoint.sh
+++ docker/portal-listener/portal-listener-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 
-ln --symbolic --force "/run/secrets/machine_secret" "/etc/machine.secret"
+ln --symbolic --force "${MACHINE_SECRET_FILE:-/run/secrets/machine_secret}" "/etc/machine.secret"
+
+if [ ! -e /etc/machine.secret ]
+then
+    echo "ERROR: MACHINE_SECRET_FILE is not set correctly."
+    exit 1
+fi
 
 exec "$@"
