1116026f9bd8af0df59894519bad83be7b0e35f6
Johannes Lohmer <lohmer@univention.de>
2023-06-20 22:23:59 +0200

    refactor(playwright): extract login/logout assertion into HomePage classes

--- tests/e2e/tests/test_login.py
+++ tests/e2e/tests/test_login.py
@@ -7,9 +7,6 @@ def test_login(navigate_to_login_page, username, password):
     page = navigate_to_login_page
     login_page = LoginPage(page)
     login_page.login(username, password)
+
     home_page_logged_in = HomePageLoggedIn(page)
-    # TODO: We are only checking the login state here since there doesn't seem to be
-    # anything common between the UCS and SouvAP envs that's unique to the home page's logged-in state
-    home_page_logged_in.reveal_right_side_menu()
-    expect(home_page_logged_in.right_side_menu.logout_button).to_be_visible()
-    expect(home_page_logged_in.right_side_menu.login_button).to_be_hidden()
+    home_page_logged_in.assert_logged_in()
