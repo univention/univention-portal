b01e2ab80c10c4df3254e0f47d68680349f2cb85
Carlos García-Mauriño <garcia-maurino@univention.de>
2024-04-15 10:42:02 +0200

    fixup! test: add unit test for user self-service birthday deletion

--- frontend/tests/unit/views/selfservice/helper.spec.ts
+++ frontend/tests/unit/views/selfservice/helper.spec.ts
@@ -1,3 +1,8 @@
+/**
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * SPDX-FileCopyrightText: 2024 Univention GmbH
+ */
+
 import { WidgetDefinition } from '@/jsHelper/forms';
 import { sanitizeFrontendValues } from '@/views/selfservice/helper.ts';
 
