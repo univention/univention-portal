d721d4e9cc9e62ea00cf4c220553c92854b35f77
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-04-24 14:56:13 +0200

    fix(listener): Skip rebuild of group membership cache in init container
    This is not needed anymore because the state is now persisted in a volume.

--- docker/portal-listener/initialize.sh
+++ docker/portal-listener/initialize.sh
@@ -4,8 +4,5 @@
 
 set -euxo pipefail
 
-# Ensure that the group membership cache is filled
-/usr/share/univention-group-membership-cache/univention-ldap-cache rebuild
-
 # Ensure that groups and portal data including assets are in the store
 univention-portal --log-stream update --reason force
