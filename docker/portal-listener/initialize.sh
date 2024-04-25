57a494b1e9142aa0384cd778a3f71eca3a0aeea3
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-04-25 15:36:02 +0200

    fix: Revert "fix(listener): Skip rebuild of group membership cache in init container"
    This reverts commit 758e424067a417b18e4547cc7cf52e60e19938c4.
    
    The change did introduce new obstacles because the portal would be empty until
    the portal listener did have enough time to process all events of the bootstrap
    jobs. Technically this would not be wrong, still, it does lead to a bad
    experience when doing evaluation deployments on slower clusters.
    
    A smarter handling of the cache rebuild will have to come once we did build the
    transition into the provisioning system.

--- docker/portal-listener/initialize.sh
+++ docker/portal-listener/initialize.sh
@@ -4,5 +4,8 @@
 
 set -euxo pipefail
 
+# Ensure that the group membership cache is filled
+/usr/share/univention-group-membership-cache/univention-ldap-cache rebuild
+
 # Ensure that groups and portal data including assets are in the store
 univention-portal --log-stream update --reason force
