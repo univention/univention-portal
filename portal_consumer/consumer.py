8967ef6b80d2c05fe6466c0394ab0df21e68d34b
Daniel Troeder <troeder@univention.de>
2024-08-12 15:27:54 +0200

    feat: log processing time

--- portal_consumer/consumer.py
+++ portal_consumer/consumer.py
@@ -3,48 +3,55 @@
 
 import asyncio
 import logging
-import os
 import subprocess
-
-from group_membership_cache import GroupMembershipCache
+import time
+from importlib.metadata import version
 
 from univention.portal.util import get_portal_update_call
-from univention.provisioning.consumer import MessageHandler, ProvisioningConsumerClient
+from univention.provisioning.consumer import MessageHandler, ProvisioningConsumerClient, ProvisioningConsumerClientSettings
 from univention.provisioning.models import Message
+
+from .group_membership_cache import GroupMembershipCache
+
+
+LOG_FORMAT = "%(asctime)s %(levelname)-5s [%(module)s.%(funcName)s:%(lineno)d] %(message)s"
+
+
+logger = logging.getLogger(__name__)
 
 
 class PortalConsumer:
-    def __init__(self):
-        logging.basicConfig(level=os.getenv("LOG_LEVEL"))
-        self.logger = logging.getLogger(__name__)
-        self._group_cache = GroupMembershipCache()
-        self._topics = (
-            "groups/group",
-            "portals/portal",
-            "portals/category",
-            "portals/entry",
-            "portals/folder",
-            "portals/announcement",
-        )
+    topics = {
+        "groups/group",
+        "portals/announcement",
+        "portals/category",
+        "portals/entry",
+        "portals/folder",
+        "portals/portal",
+    }
 
-    async def start_listening_for_changes(self) -> None:
-        self.logger.info("Listening for changes in the portal and groups")
+    def __init__(self):
+        self._group_cache = GroupMembershipCache()
+
+    async def listen_for_changes(self) -> None:
+        logger.info("Listening for changes in topics: %r", self.topics)
         async with ProvisioningConsumerClient() as client:
-            await MessageHandler(
-                client, [self.handle_message],
-            ).run()
+            await MessageHandler(client, [self.handle_message]).run()
+        logger.info("Shutting down.")
 
     async def handle_message(self, message: Message):
         topic = message.topic
-        if topic not in self._topics:
-            self.logger.warning("Detected a message with the wrong topic in the queue: %s", topic)
+        if topic not in self.topics:
+            logger.warning("Ignoring a message in the queue with the wrong topic: %r", topic)
             return
 
         body = message.body
-        self.logger.debug("Received the message with the body: %s", body)
+        logger.debug("Received a message with the body: %r", body)
 
         if topic == "groups/group":
-            self._group_cache.update_cache(body)
+            t0 = time.perf_counter()
+            self._group_cache.update_cache(body.new, body.old)
+            logger.info("Updated group cache in %.1f ms.", (time.perf_counter() - t0) * 1000)
             reason = "ldap:group"
         else:
             obj = body.new or body.old
@@ -52,14 +59,15 @@ class PortalConsumer:
             module = topic.split('/')[-1]
             reason = f"ldap:{module}:{dn}"
 
-        self.logger.info("Updating portal. Reason: %s", reason)
+        logger.debug("Updating portal. Reason: %r", reason)
+        t0 = time.perf_counter()
         subprocess.call(get_portal_update_call(reason=reason))
-
-
-def run() -> None:
-    consumer = PortalConsumer()
-    asyncio.run(consumer.start_listening_for_changes())
+        logger.info("Updated portal in %.1f ms.", (time.perf_counter() - t0) * 1000)
 
 
 if __name__ == "__main__":
-    run()
+    settings = ProvisioningConsumerClientSettings()
+    logging.basicConfig(format=LOG_FORMAT, level=settings.log_level)
+    logger.info("Using 'nubus-provisioning-consumer' library version %r.", version("nubus-provisioning-consumer"))
+    consumer = PortalConsumer()
+    asyncio.run(consumer.listen_for_changes())
