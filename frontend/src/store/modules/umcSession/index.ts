3009f874e9dbb6f2a011394aba3e48df70950d29
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-09-30 02:23:01 +0200

    feat(frontend): Add "startSessionRefresh" into umcSession store module

--- frontend/src/store/modules/umcSession/index.ts
+++ frontend/src/store/modules/umcSession/index.ts
@@ -3,36 +3,58 @@
  * SPDX-FileCopyrightText: 2023-2024 Univention GmbH
  */
 
-import { ActionContext } from 'vuex';
-
 import { PortalActionContext, PortalModule } from '../../root.models';
+import { umcGetSessionInfo, UmcSessionInfo } from './utils';
 
 export interface UmcSessionState {
+  refreshNeeded: boolean,
 }
 
 export type UmcSessionActionContext = PortalActionContext<UmcSessionState>;
+
+export const refreshBeforeExpiry = 5;
+
+export const actions = {
+  async startSessionRefresh({ dispatch }: UmcSessionActionContext): Promise<void> {
+    let result : UmcSessionInfo | undefined;
+    let refreshInSeconds = 0;
+
+    try {
+      result = await umcGetSessionInfo();
+    } catch (error) {
+      console.warn('Fetching UMC session information failed, disabling passive session refresh.');
+      return;
+    }
+    if (result) {
+      refreshInSeconds = result.remaining - refreshBeforeExpiry;
+    }
+    setTimeout(() => dispatch('refreshNeeded'), refreshInSeconds * 1000);
+  },
+
+  refreshNeeded({ commit }: UmcSessionActionContext): void {
+    commit('refreshNeeded');
+  },
+};
 
 const umcSession: PortalModule<UmcSessionState> = {
 
   namespaced: true,
 
   state: {
+    refreshNeeded: false,
   },
 
   mutations: {
+    refreshNeeded(state: UmcSessionState): void {
+      state.refreshNeeded = true;
+    },
   },
 
   getters: {
+    refreshNeeded: (state) => state.refreshNeeded,
   },
 
-  actions: {
-    /**
-     * Start the regular refresh of the UMC Session.
-     */
-    startSessionRefresh({ dispatch, getters }: UmcSessionActionContext): void {
-      console.warn('TODO: implement me!');
-    },
-  },
+  actions,
 };
 
 export default umcSession;
