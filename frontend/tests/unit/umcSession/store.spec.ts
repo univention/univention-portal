b36ccd98b9af2c36fd4a7f1e0af457b837735509
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-10-01 19:40:04 +0200

    feat(frontend): Add retartSessionRefresh and disableSessionRefresh actions

--- frontend/tests/unit/umcSession/store.spec.ts
+++ frontend/tests/unit/umcSession/store.spec.ts
@@ -1,4 +1,4 @@
-import { actions, refreshBeforeExpiry, UmcSessionActionContext } from '@/store/modules/umcSession';
+import { actions, mutations, refreshBeforeExpiry, UmcSessionActionContext } from '@/store/modules/umcSession';
 import * as utils from '@/store/modules/umcSession/utils';
 import * as stubs from './stubs';
 
@@ -8,6 +8,7 @@ const mockedUtils = jest.mocked(utils, true);
 jest.useFakeTimers();
 
 afterEach(() => {
+  jest.clearAllTimers();
   jest.resetAllMocks();
 });
 
@@ -15,43 +16,107 @@ afterAll(() => {
   jest.restoreAllMocks();
 });
 
-type StubActionContext = Pick<UmcSessionActionContext, 'dispatch'>
+type StubActionContext = Pick<UmcSessionActionContext, 'commit' | 'dispatch' | 'state'>
 
 describe('action startSessionRefresh', () => {
 
+  const stubActionContext: StubActionContext = {
+    state: { refreshNeeded: false },
+    commit: jest.fn(),
+    dispatch: jest.fn(),
+  };
+
   test('triggers UMC session refresh due to session expiry', async () => {
     mockedUtils.umcGetSessionInfo.mockResolvedValue(undefined);
-    const stubActionContext: StubActionContext = {
-      dispatch: jest.fn(),
-    };
     await actions.startSessionRefresh(stubActionContext as UmcSessionActionContext);
     jest.runAllTimers();
-    expect(stubActionContext.dispatch).toHaveBeenCalledWith('refreshNeeded');
+    expect(stubActionContext.commit).toHaveBeenCalledWith('refreshNeeded', true);
   });
 
   test('logs warning in failure case', async () => {
     const warnMock = jest.spyOn(console, 'warn');
     mockedUtils.umcGetSessionInfo.mockRejectedValue(new Error('Stub Error'));
-    const stubActionContext: StubActionContext = {
-      dispatch: jest.fn(),
-    };
     await actions.startSessionRefresh(stubActionContext as UmcSessionActionContext);
     expect(warnMock).toHaveBeenCalled();
   });
 
   test('triggers passive session refresh based on umc session expiry', async () => {
     mockedUtils.umcGetSessionInfo.mockResolvedValue(stubs.stubUmcSessionInfo);
-    const stubActionContext: StubActionContext = {
-      dispatch: jest.fn(),
-    };
 
     await actions.startSessionRefresh(stubActionContext as UmcSessionActionContext);
 
     const expectedRefresh = stubs.stubUmcSessionInfo.remaining - refreshBeforeExpiry;
     jest.advanceTimersByTime((expectedRefresh - 1) * 1000);
-    expect(stubActionContext.dispatch).not.toHaveBeenCalledWith('refreshNeeded');
+    expect(stubActionContext.commit).not.toHaveBeenCalledWith('refreshNeeded', true);
     jest.advanceTimersByTime(1 * 1000);
-    expect(stubActionContext.dispatch).toHaveBeenCalledWith('refreshNeeded');
+    expect(stubActionContext.commit).toHaveBeenCalledWith('refreshNeeded', true);
+  });
+
+  test('resets pending timer', async () => {
+    mockedUtils.umcGetSessionInfo.mockResolvedValue(stubs.stubUmcSessionInfo);
+    const refreshNeededMock = jest.spyOn(mutations, 'refreshNeeded');
+    const stubStore = stubs.createStubStore();
+
+    await stubStore.dispatch('umcSession/startSessionRefresh');
+    jest.advanceTimersByTime(5 * 1000);
+    await stubStore.dispatch('umcSession/startSessionRefresh');
+    jest.advanceTimersByTime((stubs.stubUmcSessionInfo.remaining + 1) * 1000);
+
+    expect(refreshNeededMock).toHaveBeenCalledTimes(1);
+  });
+
+});
+
+describe('action restartSessionRefresh', () => {
+
+  test('disables refreshNeeded', async () => {
+    const refreshNeededMock = jest.spyOn(mutations, 'refreshNeeded');
+    const stubStore = stubs.createStubStore({ refreshNeeded: true });
+    await stubStore.dispatch('umcSession/restartSessionRefresh')
+
+    expect(refreshNeededMock).toHaveBeenCalledWith(expect.anything(), false);
+  });
+
+  test('starts a new session refresh', async () => {
+    const startSessionRefreshMock = jest.spyOn(actions, 'startSessionRefresh');
+    const stubStore = stubs.createStubStore({ refreshNeeded: true });
+    await stubStore.dispatch('umcSession/restartSessionRefresh')
+
+    expect(startSessionRefreshMock).toHaveBeenCalled();
+  });
+
+});
+
+describe('action disableSessionRefresh', () => {
+
+  test('disables refreshNeeded', async () => {
+    const refreshNeededMock = jest.spyOn(mutations, 'refreshNeeded');
+    const stubStore = stubs.createStubStore({ refreshNeeded: true });
+    await stubStore.dispatch('umcSession/disableSessionRefresh')
+
+    expect(refreshNeededMock).toHaveBeenCalledWith(expect.anything(), false);
+  });
+
+  test('starts a new session refresh', async () => {
+    const startSessionRefreshMock = jest.spyOn(actions, 'startSessionRefresh');
+    const stubStore = stubs.createStubStore({ refreshNeeded: true });
+    await stubStore.dispatch('umcSession/disableSessionRefresh')
+
+    expect(startSessionRefreshMock).not.toHaveBeenCalled();
+  });
+
+  test('resets pending timer', async () => {
+    mockedUtils.umcGetSessionInfo.mockResolvedValue(stubs.stubUmcSessionInfo);
+    const refreshNeededMock = jest.spyOn(mutations, 'refreshNeeded');
+    const stubStore = stubs.createStubStore();
+
+    await stubStore.dispatch('umcSession/startSessionRefresh');
+    jest.advanceTimersByTime(5 * 1000);
+    await stubStore.dispatch('umcSession/disableSessionRefresh');
+    jest.advanceTimersByTime((stubs.stubUmcSessionInfo.remaining + 1) * 1000);
+
+    expect(refreshNeededMock).toHaveBeenCalledTimes(1);
+    expect(refreshNeededMock).toHaveBeenCalledWith(expect.anything(), false);
   });
 
 });
