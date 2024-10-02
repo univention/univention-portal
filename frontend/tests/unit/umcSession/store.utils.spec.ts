2f219b9a40a070dff55824a5fa079866e1cd8228
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-10-02 18:06:05 +0200

    refactor(frontend): Remove the Jest 24 compatibility tweaks again
    We did want to merge the UMC Session refresh before upgrading dependencies. To
    make it possible a few workarounds for Jest 24 compatibility were added.
    
    This reverts commit b4e477908ccc4775998eb9b2aa23f6545b3c34ac.

--- frontend/tests/unit/umcSession/store.utils.spec.ts
+++ frontend/tests/unit/umcSession/store.utils.spec.ts
@@ -23,10 +23,7 @@ describe('umcGetSessionInfo', () => {
   test('triggers POST request to ".../get/session-info"', async () => {
     mockedPost.mockResolvedValue(stubs.stubSessionInfoResponse);
     await umcGetSessionInfo();
-    // TODO: Refactor once Jest >=27 is available
-    // expect(mockedPost.mock.lastCall[0]).toBe('/univention/get/session-info');
-    const lastCall = mockedPost.mock.calls[mockedPost.mock.calls.length - 1];
-    expect(lastCall[0]).toBe('/univention/get/session-info');
+    expect(mockedPost.mock.lastCall[0]).toBe('/univention/get/session-info');
   });
 
   test('returns umc response', async () => {
