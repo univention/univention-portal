univention/univention-corporate-server

style: convert tabs to spaces in Python files
autopep8 -i --select E1,W191 -j0 --ignore-local-config $(python_files) $(find -name '*.pyi')

$ autopep8 --version
autopep8 1.7.1 (pycodestyle: 2.9.1)

(actually used fixers: E101,E123,E124,E126,E127,E128,E131,W191)

Bug #31771

management/univention-portal/unittests/test_log.py
@@ -38,8 +38,8 @@ from univentionunittests import import_module
 
 
 def test_setup_logger(portal_lib, request):
-	use_installed = request.config.getoption("--installed-portal")
-	module = import_module("univention.portal.log", "python/", "univention.portal.log", use_installed=use_installed)
-	module.setup_logger()
-	unittest_logger = module.get_logger("unittest")
-	unittest_logger.info("test_setup_logger works")
+    use_installed = request.config.getoption("--installed-portal")
+    module = import_module("univention.portal.log", "python/", "univention.portal.log", use_installed=use_installed)
+    module.setup_logger()
+    unittest_logger = module.get_logger("unittest")
+    unittest_logger.info("test_setup_logger works")

univention/univention-portal

unittests/test_log.py
@@ -35,8 +35,8 @@
 
 
 def test_setup_logger():
-	from univention.portal import log
+    from univention.portal import log
 
-	log.setup_logger()
-	unittest_logger = log.get_logger("unittest")
-	unittest_logger.info("test_setup_logger works")
+    log.setup_logger()
+    unittest_logger = log.get_logger("unittest")
+    unittest_logger.info("test_setup_logger works")