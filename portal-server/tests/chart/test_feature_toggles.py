d88bfad47be32485ca80937de87453493b94f10e
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-02-13 16:16:19 +0100

    fix(server): Correct handling of the case when all toggles are switched off
    This case turned out to be tricky.
    
    1. Setting the following value does not change anything:
    
       ```
       portalServer:
         featureToggles: {}
       ```
    
       This is most likely due to the way how the Helm merge works.
    
    2. The following two configurations both set the value to "null":
    
       ```
       portalServer:
         featureToggles:
       ```
    
       ```
       portalServer:
         featureToggles: null
       ```
    
       In this case the environment variable will not be added into the ConfigMap.

---
+++ portal-server/tests/chart/test_feature_toggles.py
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+import json
+
+import pytest
+from yaml import safe_load
+
+from utils import findone
+
+
+@pytest.mark.parametrize("value", ["", "null"])
+def test_disabling_all_feature_toggles(helm, chart_path, value):
+    values = safe_load(
+        f"""
+        portalServer:
+          featureToggles: {value}
+    """,
+    )
+    result = helm.helm_template(chart_path, values)
+    configmap = helm.get_resource(result, kind="ConfigMap")
+    assert "PORTAL_SERVER_FEATURE_TOGGLES" not in configmap["data"]
+
+
+def test_allows_to_add_arbitrary_feature_toggles(helm, chart_path):
+    values = safe_load(
+        """
+        portalServer:
+          featureToggles:
+            test_feature_a: true
+            test_feature_b: false
+    """,
+    )
+    result = helm.helm_template(chart_path, values)
+    configmap = helm.get_resource(result, kind="ConfigMap")
+    feature_toggles = json.loads(findone(configmap, "data.PORTAL_SERVER_FEATURE_TOGGLES"))
+    assert feature_toggles["test_feature_a"] is True
+    assert feature_toggles["test_feature_b"] is False
