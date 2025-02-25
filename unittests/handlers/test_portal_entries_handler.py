872804ae4216bcc5592100f26bb03f267bde2845
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-02-25 16:50:31 +0100

    test(portal-server): Move fixtures into conftest

--- unittests/handlers/test_portal_entries_handler.py
+++ unittests/handlers/test_portal_entries_handler.py
@@ -41,48 +41,6 @@ import tornado.testing
 import tornado.web
 
 from univention.portal.main import build_routes
-from univention.portal.user import User
-
-
-def async_method_patch(mocker, callable):
-    mocker.MagicMock.__await__ = lambda _: callable().__await__()
-    return mocker.MagicMock()
-
-
-@pytest.fixture()
-def user():
-    return User(
-        username=None,
-        display_name=None,
-        groups=[],
-        headers={},
-    )
-
-
-@pytest.fixture()
-def portal_mock(mocker, user):
-    async def get_user():
-        return user
-
-    portal = mocker.Mock()
-    portal.portal_cache = None
-    portal.score = mocker.Mock(return_value=1)
-    portal.get_user = async_method_patch(mocker, get_user)
-    portal.refresh = mocker.Mock()
-    portal.get_cache_id = mocker.Mock(return_value=None)
-    portal.get_visible_content = mocker.Mock(return_value=None)
-    portal.get_user_links = mocker.Mock(return_value=None)
-    portal.get_menu_links = mocker.Mock(return_value=None)
-    portal.get_entries = mocker.Mock(return_value=None)
-    portal.get_folders = mocker.Mock(return_value=None)
-    portal.get_categories = mocker.Mock(return_value=None)
-    portal.get_meta = mocker.Mock(return_value={"showUmc": False})
-    portal.auth_mode = mocker.Mock(return_value=None)
-    portal.may_be_edited = mocker.Mock(return_value=None)
-    portal.get_announcements = mocker.Mock(return_value=None)
-    portal.get_feature_toggles = mocker.Mock(return_value={})
-
-    return portal
 
 
 class TestPortalEntriesHandlerNoHttpCache:
