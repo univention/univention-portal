e374416e6c5d051c969f9e3b25dcfb02525b743c
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-02-26 16:21:03 +0100

    refactor(portal-server): Add logging into api me endpoint

--- python/univention/portal/handlers/api_me.py
+++ python/univention/portal/handlers/api_me.py
@@ -1,14 +1,15 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
+import logging
+
 import tornado.web
 
 from univention.portal.handlers.portal_resource import PortalResource
-from univention.portal.log import get_logger
 from univention.portal.udm import AsyncUdmClient
 
 
-logger = get_logger("handlers.api_me")
+logger = logging.getLogger(__name__)
 
 
 class ApiMeHandler(PortalResource):
@@ -27,14 +28,18 @@ class ApiMeHandler(PortalResource):
 
         portal = self.find_portal()
         if not portal:
+            logger.error("Portal for current request not found.")
             raise tornado.web.HTTPError(404)
 
         answer = {}
         user = await portal.get_user(self)
 
         if user.is_logged_in():
+            logger.debug("User is logged in, fetching user details from UDM Rest API.")
             udm_user_data = await self.udm_client.get_user(user.username)
             answer.update(_map_from_udm_model(udm_user_data))
+        else:
+            logger.debug("Anonymous user, returning an empty object.")
 
         self.write(answer)
 
