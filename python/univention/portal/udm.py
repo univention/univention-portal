cc90c98c4fa3caa16ca35bb39edd4b9757324a3b
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-02-26 12:35:32 +0100

    feat(portal-server): Solidify the AsyncUdmClient implementation

--- python/univention/portal/udm.py
+++ python/univention/portal/udm.py
@@ -11,23 +11,23 @@ class AsyncUdmClient:
     """Minimal utility to request a user from the UDM Rest API."""
 
     def __init__(self, udm_api_url: str, username: str, password: str):
-        self._udm_api_url = udm_api_url
+        self._udm_api_url = _ensure_trailing_slash(udm_api_url)
         self._username = username
         self._password = password
 
     async def get_user(self, username: str):
-        # TODO: utility to construct search URL
-        # TODO: Be strict about the username requirements, e.g. not None, length > 0
+        if not username:
+            raise ValueError("The username cannot be empty.")
         query_string = urlencode({"filter": f"(username={username})"})
         udm_query = f"users/user/?{query_string}"
         data = await self._fetch_from_udm(udm_query)
         if data["results"] != 1:
-            raise RuntimeError("Fetching the user's data failed")
+            raise UnexpectedResult("Fetching the user's data failed")
         user_data = data["_embedded"]["udm:object"][0]
         return user_data
 
     async def _fetch_from_udm(self, udm_query: str):
-        url = urljoin(self._udm_api_url, udm_query)
+        url = self._build_full_url(udm_query)
         http_client = AsyncHTTPClient()
         response = await http_client.fetch(
             url,
@@ -37,3 +37,15 @@ class AsyncUdmClient:
         )
         data = json.loads(response.body)
         return data
+
+    def _build_full_url(self, sub_path):
+        url = urljoin(self._udm_api_url, sub_path)
+        return url
+
+
+def _ensure_trailing_slash(value: str):
+    return value.rstrip("/") + "/"
+
+
+class UnexpectedResult(Exception):
+    pass
---
+++ unittests/test_udm.py
@@ -0,0 +1,115 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+import json
+from unittest import mock
+
+import pytest
+
+from univention.portal.udm import AsyncUdmClient, UnexpectedResult
+
+
+@pytest.fixture()
+def udm_user_stub():
+    return {
+        "id": "stub-id",
+    }
+
+
+@pytest.fixture()
+def AsyncHTTPClient_stub(udm_user_stub):
+    stub = mock.Mock()
+    stub().fetch = mock.AsyncMock()
+    response = mock.Mock()
+    response.body = json.dumps({
+        "results": 1,
+        "_embedded": {
+            "udm:object": [udm_user_stub],
+        },
+    })
+    stub().fetch.return_value = response
+    return stub
+
+
+@pytest.fixture(autouse=True)
+def mock_http_client(AsyncHTTPClient_stub, mocker):
+    mocker.patch("univention.portal.udm.AsyncHTTPClient", AsyncHTTPClient_stub)
+    return AsyncHTTPClient_stub
+
+
+@pytest.mark.asyncio()
+async def test_get_user_returns_embedded_object(udm_user_stub):
+    udm_client = AsyncUdmClient("stub_url", "stub_user", "stub_password")
+    data = await udm_client.get_user("username")
+    assert data == udm_user_stub
+
+
+@pytest.mark.asyncio()
+@pytest.mark.parametrize("results_count", [0, 2])
+async def test_get_user_ensures_exactly_one_result(mocker, udm_user_stub, results_count):
+    udm_client = AsyncUdmClient("stub_url", "stub_user", "stub_password")
+    fetch_from_udm_mock = mocker.patch.object(udm_client, "_fetch_from_udm")
+    fetch_from_udm_mock.return_value = {
+        "results": results_count,
+        "_embedded": {
+            "udm:object": [udm_user_stub] * results_count,
+        },
+    }
+    with pytest.raises(UnexpectedResult):
+        await udm_client.get_user("username")
+
+
+@pytest.mark.asyncio()
+@pytest.mark.parametrize("response_body, ExpectedException", [
+    ("invalid", json.JSONDecodeError),
+    ("{}", KeyError),
+    ('{"results":1}', KeyError),
+    ("<html><div>stub result</div></html>", json.JSONDecodeError),
+])
+async def test_get_user_raises_on_unexpected_response(
+    mock_http_client, response_body, ExpectedException,
+):
+    (await mock_http_client().fetch()).body = response_body
+    udm_client = AsyncUdmClient("stub_url", "stub_user", "stub_password")
+    with pytest.raises(ExpectedException):
+        await udm_client.get_user("username")
+
+
+@pytest.mark.asyncio()
+@pytest.mark.parametrize("username, ExpectedException", [
+    ("", ValueError),
+    (None, ValueError),
+])
+async def test_get_user_validates_username(
+    mock_http_client, username, ExpectedException,
+):
+    udm_client = AsyncUdmClient("stub_url", "stub_user", "stub_password")
+    with pytest.raises(ExpectedException):
+        await udm_client.get_user(username)
+
+
+@pytest.mark.asyncio()
+async def test_fetch_from_udm_requsets_user_data(mock_http_client):
+    udm_client = AsyncUdmClient("stub_url/", "stub_user", "stub_password")
+    await udm_client._fetch_from_udm("stub_udm_query")
+    mock_http_client().fetch.assert_called_with(
+        "stub_url/stub_udm_query",
+        auth_username="stub_user",
+        auth_password="stub_password",
+        headers={"accept": "application/json"},
+    )
+
+
+@pytest.mark.asyncio()
+@pytest.mark.parametrize("udm_api_url, expected_url", [
+    ("", "/stub_udm_query"),
+    ("/", "/stub_udm_query"),
+    ("stub_url", "stub_url/stub_udm_query"),
+    ("stub_url/", "stub_url/stub_udm_query"),
+    ("/stub_url", "/stub_url/stub_udm_query"),
+    ("/stub_url/", "/stub_url/stub_udm_query"),
+])
+async def test_async_udm_client_ensures_trailing_slash(mock_http_client, udm_api_url, expected_url):
+    udm_client = AsyncUdmClient(udm_api_url, "stub_user", "stub_password")
+    url = udm_client._build_full_url("stub_udm_query")
+    assert url == expected_url
