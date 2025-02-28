23c0da79df10f4cf61382b7c60fbb84ee16d03eb
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-02-28 13:45:16 +0100

    feat(portal-frontend): Add path of the "me" endpoint from the portal api

--- frontend/src/store/utils.ts
+++ frontend/src/store/utils.ts
@@ -12,6 +12,7 @@ export const portalUrl = process.env.VUE_APP_PORTAL_URL || '';
 export const languageJsonPath = process.env.VUE_APP_LANGUAGE_DATA || '/univention/languages.json';
 export const portalJsonPath = process.env.VUE_APP_PORTAL_DATA || './portal.json';
 export const portalMetaPath = process.env.VUE_APP_META_DATA || '/univention/meta.json';
+export const portalApiMePath = process.env.VUE_APP_PORTAL_API_ME || './api/v1/me';
 
 export async function portalJsonRequest(adminMode: boolean) {
   const umcLang = getCookie('UMCLang');
