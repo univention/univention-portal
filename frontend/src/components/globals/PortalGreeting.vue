5bc41bfdfaa1e7d59be7d16bd27cb297102b04fe
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-03-18 12:07:17 +0100

    chore(portal-frontend): Add license header into the new source files

--- frontend/src/components/globals/PortalGreeting.vue
+++ frontend/src/components/globals/PortalGreeting.vue
@@ -1,3 +1,8 @@
+<!--
+ SPDX-License-Identifier: AGPL-3.0-only
+ SPDX-FileCopyrightText: 2025 Univention GmbH
+-->
+
 <template>
   <template v-if="showWelcomeMessage && userFirstname.length > 0">
     <div class="portal-greeting">
