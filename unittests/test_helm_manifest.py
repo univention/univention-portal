757c43aaf5fef3ce06fdc87153a8fbfa29f8af4d
Jan Burgmeier <jan.burgmeier.extern@univention.de>
2025-03-26 18:37:21 +0100

    fix(helm portal-consumer): Add helm-test-harness to unit tests
    Make sure helm manifest is coorect according to the helm-test-harness
    
    univention/dev/internal/team-nubus#1096

---
+++ unittests/test_helm_manifest.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import pytest
+from yaml import safe_load
+
+from univention.testing.helm.deployment import Deployment
+
+
+def get_absolute_path(path: Path):
+    base_path = Path(__file__).parent.parent
+    return str(base_path / path)
+
+
+@pytest.fixture()
+def helm_values():
+    """Use "helm/directory-consumer/linter_values.yaml" as default values."""
+    return [get_absolute_path("helm/portal-consumer/linter_values.yaml")]
+
+
+@pytest.fixture()
+def chart_path():
+    """Path to the Helm chart which shall be tested."""
+    return get_absolute_path("helm/portal-consumer")
+
+
+class TestDeployment(Deployment):
+    template_file = "templates/statefulset.yaml"
