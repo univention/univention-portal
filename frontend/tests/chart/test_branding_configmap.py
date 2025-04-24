0bc3989906c598ce47fd46e8e41b6c94d37d9006
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-04-24 10:22:25 +0200

    test: Prepare branding related tests for parametrization

--- frontend/tests/chart/test_branding_configmap.py
+++ frontend/tests/chart/test_branding_configmap.py
@@ -1,47 +1,53 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
+import pytest
 from pytest_helm import utils
 from yaml import safe_load
 
 
-def test_favicon_not_added_by_default(helm, chart_path):
+pytestmark = pytest.mark.parametrize("values_key,filename", [
+    ("favicon", "favicon.ico"),
+])
+
+
+def test_file_not_added_by_default(values_key, filename, helm, chart_path):
     values = {}
     result = helm.helm_template(chart_path, values, template_file="templates/branding-configmap.yaml")
     config_map = helm.get_resource(result, kind="ConfigMap")
-    assert "favicon.ico" not in config_map["binaryData"]
+    assert filename not in config_map["binaryData"]
 
 
-def test_favicon_is_not_mounted_by_default(helm, chart_path):
+def test_file_is_not_mounted_by_default(values_key, filename, helm, chart_path):
     values = {}
     result = helm.helm_template(chart_path, values)
     deployment = helm.get_resource(result, kind="Deployment")
     volume_mounts = utils.findone(
         deployment, "spec.template.spec.containers[?@.name=='portal-frontend'].volumeMounts")
-    favicon_mount = [mount for mount in volume_mounts if mount.get("subPath") == "favicon.ico"]
-    assert not favicon_mount
+    file_mount = [mount for mount in volume_mounts if mount.get("subPath") == filename]
+    assert not file_mount
 
 
-def test_favicon_is_added_when_configured(helm, chart_path):
-    values = safe_load("""
+def test_file_is_added_when_configured(values_key, filename, helm, chart_path):
+    values = safe_load(f"""
         portalFrontend:
           branding:
-            favicon: stub_value
+            {values_key}: stub_value
         """)
     result = helm.helm_template(chart_path, values, template_file="templates/branding-configmap.yaml")
     config_map = helm.get_resource(result, kind="ConfigMap")
-    assert config_map["binaryData"]["favicon.ico"] == "stub_value"
+    assert config_map["binaryData"][filename] == "stub_value"
 
 
-def test_favicon_is_mounted_when_configured(helm, chart_path):
-    values = safe_load("""
+def test_file_is_mounted_when_configured(values_key, filename, helm, chart_path):
+    values = safe_load(f"""
         portalFrontend:
           branding:
-            favicon: stub_value
+            {values_key}: stub_value
         """)
     result = helm.helm_template(chart_path, values)
     deployment = helm.get_resource(result, kind="Deployment")
     volume_mounts = utils.findone(
         deployment, "spec.template.spec.containers[?@.name=='portal-frontend'].volumeMounts")
-    favicon_mount = [mount for mount in volume_mounts if mount.get("subPath") == "favicon.ico"]
-    assert favicon_mount
+    file_mount = [mount for mount in volume_mounts if mount.get("subPath") == filename]
+    assert file_mount
