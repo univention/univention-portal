b6ebce9bdd5d6792a1c98e760beb92f05e64a034
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-05-21 23:40:38 +0200

    test(portal-server): Use "get_containers" from pytest_helm.utils
    Issue: univention/dev/internal/team-nubus#892

--- portal-server/tests/chart/utils.py
+++ portal-server/tests/chart/utils.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 # SPDX-License-Identifier: AGPL-3.0-only
 
+from pytest_helm.utils import get_containers
+
 
 def get_containers_of_job(result):
     return _get_containers_of("Job", result)
@@ -12,6 +14,4 @@ def get_containers_of_deployment(result):
 
 def _get_containers_of(kind, result):
     manifest = result.get_resource(kind=kind)
-    init_containers = manifest.findone("spec.template.spec.initContainers")
-    containers = manifest.findone("spec.template.spec.containers")
-    return init_containers + containers
+    return get_containers(manifest)
