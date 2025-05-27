14a59457eeeb2c879b0c8648b4787d0f5c0be2c3
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-05-27 10:45:57 +0200

    test(portal-consumer): Add best practice tests for the Helm chart
    Issue: univention/dev/internal/team-nubus#892

---
+++ portal-consumer/tests/chart/best_practices/test_image_configuration.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+
+from univention.testing.helm.best_practice.image_configuration import ImageConfiguration
+
+
+class TestImageConfiguration(ImageConfiguration):
+    pass
