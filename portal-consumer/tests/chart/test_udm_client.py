c5851427312512a41eed197db9ebc74c84564055
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-05-27 22:16:57 +0200

    feat(portal-consumer): UDM client configuration follows secrets structure
    Issue: univention/dev/internal/team-nubus#892

---
+++ portal-consumer/tests/chart/test_udm_client.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from univention.testing.helm.client.udm import UdmClient
+
+
+class TestUdmClientConfiguration(UdmClient):
+
+    config_map_name = "release-name-portal-consumer"
+    secret_name = "release-name-portal-consumer-udm"
+    workload_resource_kind = "StatefulSet"
+
+    default_username = "cn=admin"
+
+    path_udm_api_url = "data.PORTAL_UDM_API_URL"
+    path_udm_api_username = "data.PORTAL_UDM_API_USERNAME"
+    path_main_container = "spec.template.spec.containers[?@.name=='portal-consumer']"
