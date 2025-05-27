738038a3875d2ca7ba8302465c31abb835ef5607
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-05-27 23:03:49 +0200

    test(portal-consumer): Adjust config map name in ldap related tests
    Issue: univention/dev/internal/team-nubus#892

--- portal-consumer/tests/chart/test_ldap_client.py
+++ portal-consumer/tests/chart/test_ldap_client.py
@@ -1,9 +1,12 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
 from univention.testing.helm.client.ldap import Ldap
 
 
 class TestLdapClient(Ldap):
 
-    config_map_name = "release-name-portal-consumer-common"
+    config_map_name = "release-name-portal-consumer"
     secret_name = "release-name-portal-consumer-ldap"
 
     path_main_container = "spec.template.spec.containers[?@.name=='portal-consumer']"
