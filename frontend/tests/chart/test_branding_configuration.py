b09272b181df0076b7d8b3feac1a6f89f360e133
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-05-28 10:41:04 +0200

    test(portal-frontend): Use the fixture "chart"
    Issue: univention/dev/internal/team-nubus#892

--- frontend/tests/chart/test_branding_configuration.py
+++ frontend/tests/chart/test_branding_configuration.py
@@ -2,7 +2,6 @@
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
 import pytest
-from pytest_helm import utils
 from yaml import safe_load
 
 
@@ -16,43 +15,43 @@ pytestmark = pytest.mark.parametrize("values_key,filename", [
 ])
 
 
-def test_file_not_added_by_default(values_key, filename, helm, chart_path):
+def test_file_not_added_by_default(values_key, filename, chart):
     values = {}
-    result = helm.helm_template(chart_path, values, template_file="templates/branding-configmap.yaml")
-    config_map = helm.get_resource(result, kind="ConfigMap")
+    result = chart.helm_template(values, template_file="templates/branding-configmap.yaml")
+    config_map = result.get_resource(kind="ConfigMap")
     assert filename not in config_map["binaryData"]
 
 
-def test_file_is_not_mounted_by_default(values_key, filename, helm, chart_path):
+def test_file_is_not_mounted_by_default(values_key, filename, chart):
     values = {}
-    result = helm.helm_template(chart_path, values)
-    deployment = helm.get_resource(result, kind="Deployment")
-    volume_mounts = utils.findone(
-        deployment, "spec.template.spec.containers[?@.name=='portal-frontend'].volumeMounts")
+    result = chart.helm_template(values)
+    deployment = result.get_resource(kind="Deployment")
+    volume_mounts = deployment.findone(
+        "spec.template.spec.containers[?@.name=='portal-frontend'].volumeMounts")
     file_mount = [mount for mount in volume_mounts if mount.get("subPath") == filename]
     assert not file_mount
 
 
-def test_file_is_added_when_configured(values_key, filename, helm, chart_path):
+def test_file_is_added_when_configured(values_key, filename, chart):
     values = safe_load(f"""
         portalFrontend:
           branding:
             {values_key}: stub_value
         """)
-    result = helm.helm_template(chart_path, values, template_file="templates/branding-configmap.yaml")
-    config_map = helm.get_resource(result, kind="ConfigMap")
+    result = chart.helm_template(values, template_file="templates/branding-configmap.yaml")
+    config_map = result.get_resource(kind="ConfigMap")
     assert config_map["binaryData"][filename] == "stub_value"
 
 
-def test_file_is_mounted_when_configured(values_key, filename, helm, chart_path):
+def test_file_is_mounted_when_configured(values_key, filename, chart):
     values = safe_load(f"""
         portalFrontend:
           branding:
             {values_key}: stub_value
         """)
-    result = helm.helm_template(chart_path, values)
-    deployment = helm.get_resource(result, kind="Deployment")
-    volume_mounts = utils.findone(
-        deployment, "spec.template.spec.containers[?@.name=='portal-frontend'].volumeMounts")
+    result = chart.helm_template(values)
+    deployment = result.get_resource(kind="Deployment")
+    volume_mounts = deployment.findone(
+        "spec.template.spec.containers[?@.name=='portal-frontend'].volumeMounts")
     file_mount = [mount for mount in volume_mounts if mount.get("subPath") == filename]
     assert file_mount
