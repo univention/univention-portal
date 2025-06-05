3e991f52155cdd7d1a812c1285db6711cd3fc1f0
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-05 20:58:15 +0200

    test(portal-consumer): Configure workload_resource_kind
    Issue: univention/dev/internal/team-nubus#892

--- portal-consumer/tests/chart/test_ldap_client.py
+++ portal-consumer/tests/chart/test_ldap_client.py
@@ -8,5 +8,6 @@ class TestLdapClient(Ldap):
 
     config_map_name = "release-name-portal-consumer"
     secret_name = "release-name-portal-consumer-ldap"
+    workload_resource_kind = "StatefulSet"
 
     path_main_container = "spec.template.spec.containers[?@.name=='portal-consumer']"
