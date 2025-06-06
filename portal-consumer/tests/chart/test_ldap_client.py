e4d0846d7fb56c283ade2516b85e39f608db408d
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-06 12:16:22 +0200

    test(portal-consumer): Update class name in ldap import, LdapAuth
    Issue: univention/dev/internal/team-nubus#892

--- portal-consumer/tests/chart/test_ldap_client.py
+++ portal-consumer/tests/chart/test_ldap_client.py
@@ -1,13 +1,18 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.ldap import Ldap
+from univention.testing.helm.client.ldap import LdapAuth, LdapConnectionHostAndPort
 
 
-class TestLdapClient(Ldap):
+class TestLdapClient(LdapAuth):
 
     config_map_name = "release-name-portal-consumer"
     secret_name = "release-name-portal-consumer-ldap"
     workload_resource_kind = "StatefulSet"
 
     path_main_container = "spec.template.spec.containers[?@.name=='portal-consumer']"
+
+
+class TestLdapConnection(LdapConnectionHostAndPort):
+
+    config_map_name = "release-name-portal-consumer"
