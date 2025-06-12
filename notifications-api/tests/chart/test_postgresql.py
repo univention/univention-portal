ec192ee6b113d770518905199469f2d2d8219860
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-12 09:55:07 +0200

    test(notifications-api): Update postgresql related tests to new helm-test-harness
    Issue: univention/dev/internal/team-nubus#892

--- notifications-api/tests/chart/test_postgresql.py
+++ notifications-api/tests/chart/test_postgresql.py
@@ -1,12 +1,15 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.postgresql import PostgresqlClient
+from univention.testing.helm.client.postgresql import PostgresqlAuth, PostgresqlConnection
 
 
-class TestPostgresqlClient(PostgresqlClient):
+class TestPostgresqlAuth(PostgresqlAuth):
 
     default_username = "notificationsapi"
     default_database = "notificationsapi"
     secret_name = "release-name-notifications-api-postgresql"
 
+
+class TestPostgresqlConnection(PostgresqlConnection):
+    pass
