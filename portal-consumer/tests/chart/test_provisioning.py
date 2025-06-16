f8263b109d7c74b61d1d995492b2b79d13dd7576
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-16 06:30:53 +0200

    test(portal-consumer): Adjust provisioning tests to latest changes in the test harness
    Issue: univention/dev/internal/team-nubus#892

--- portal-consumer/tests/chart/test_provisioning.py
+++ portal-consumer/tests/chart/test_provisioning.py
@@ -1,17 +1,23 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.provisioning_api import ProvisioningApi
+from univention.testing.helm.client.provisioning_api import Auth, Connection
 
 
-class TestProvisioningApiClientConfiguration(ProvisioningApi):
+class TestAuth(Auth):
 
-    # TODO: Check what's needed
     config_map_name = "release-name-portal-consumer"
     secret_name = "release-name-portal-consumer-provisioning-api"
     workload_kind = "StatefulSet"
 
     default_username = "portal-consumer"
 
-    path_provisioning_api_url = "data.PROVISIONING_API_BASE_URL"
     path_main_container = "spec.template.spec.containers[?@.name=='portal-consumer']"
+    path_provisioning_api_url = "data.PROVISIONING_API_BASE_URL"
+
+
+class TestConnection(Connection):
+
+    config_map_name = "release-name-portal-consumer"
+
+    path_provisioning_api_url = "data.PROVISIONING_API_BASE_URL"
