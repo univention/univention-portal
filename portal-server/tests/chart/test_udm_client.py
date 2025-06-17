7291ebffc3404fb68cea0be5dafa50049aa78165
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-17 23:22:39 +0200

    test(portal-server): Update udm tests to align with updated class structure in helm-test-harness
    Issue: univention/dev/internal/team-nubus#892

--- portal-server/tests/chart/test_udm_client.py
+++ portal-server/tests/chart/test_udm_client.py
@@ -1,13 +1,20 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.udm import UdmClient
+from univention.testing.helm.client.udm import Auth, Connection, SecretViaVolume
 
 
-class TestUdmClientConfiguration(UdmClient):
+class TestAuth(SecretViaVolume, Auth):
+    config_map_name = "release-name-portal-server"
+    secret_name = "release-name-portal-server-udm"
 
     default_username = "svc-portal-server"
-    path_udm_api_url = "data.PORTAL_UDM_API_URL"
-    path_udm_api_username = "data.PORTAL_UDM_API_USERNAME"
+
+    path_username = "data.PORTAL_UDM_API_USERNAME"
     path_main_container = "spec.template.spec.containers[?@.name=='portal-server']"
-    secret_name = "release-name-portal-server-udm"
+
+
+class TestConnection(Connection):
+    config_map_name = "release-name-portal-server"
+
+    path_url = "data.PORTAL_UDM_API_URL"
