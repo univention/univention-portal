4fc8170b8b0a7838cae8b3b8d0ff2ac650bb2466
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-19 09:32:33 +0200

    test(portal-consumer): Adjust provisioning api related tests to updated classes
    Issue: univention/dev/internal/team-nubus#892

--- portal-consumer/tests/chart/test_provisioning.py
+++ portal-consumer/tests/chart/test_provisioning.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.provisioning_api import Auth, Connection
+from univention.testing.helm.client.provisioning_api import Auth, Connection, SecretViaEnv
 
 
-class TestAuth(Auth):
+class TestAuth(SecretViaEnv, Auth):
 
     config_map_name = "release-name-portal-consumer"
     secret_name = "release-name-portal-consumer-provisioning-api"
@@ -12,7 +12,7 @@ class TestAuth(Auth):
 
     default_username = "portal-consumer"
 
-    path_main_container = "spec.template.spec.containers[?@.name=='portal-consumer']"
+    path_container = "spec.template.spec.containers[?@.name=='portal-consumer']"
     path_provisioning_api_url = "data.PROVISIONING_API_BASE_URL"
 
 
