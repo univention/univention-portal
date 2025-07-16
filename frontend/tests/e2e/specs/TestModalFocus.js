eac1bce8c3ffb0e446b768715b18f627c8614f77
Chrissy Topal <chrissy.topal.extern@univention.de>
2025-07-16 13:28:11 +0200

    feat(portal-frontend): write tests for the focusmanagement
    univention/dev/projects/portal#741

--- frontend/tests/e2e/specs/TestModalFocus.js
+++ frontend/tests/e2e/specs/TestModalFocus.js
@@ -94,5 +94,169 @@ describe('Test Modal Focus Behavior', () => {
       }
     });
   });
+
+  it('every link and button should have tabindex -1 when a modal is open', () => {
+    // Wait for page to fully load
+    cy.get('.portal-tile')
+      .should('exist')
+      .and('have.length.greaterThan', 0);
+
+    cy.get('[data-test="portal-folder"]').first()
+      .click();
+
+    // Wait for modal to open
+    cy.get('.modal-wrapper--isVisible')
+      .should('exist');
+
+    // Test portal tiles OUTSIDE the modal should have tabindex -1 (background elements)
+    cy.get('[data-test="tileLink"]')
+      .should('exist')
+      .not('.portal-tile--minified')
+      .not('.modal-wrapper--isVisible [data-test="tileLink"]') // Exclude tiles inside modal
+      .each(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'tabindex', '-1');
+      });
+
+    // Test portal tiles INSIDE the modal should have tabindex 0 (focusable in modal)
+    cy.get('.modal-wrapper--isVisible [data-test="tileLink"]')
+      .then(($modalTiles) => {
+        if ($modalTiles.length > 0) {
+          cy.wrap($modalTiles)
+            .not('.portal-tile--minified')
+            .each(($el) => {
+              cy.wrap($el)
+                .should('have.attr', 'tabindex', '0');
+            });
+        }
+      });
+
+    // Test portal-newsfeed-view-all elements (if newsfeed exists)
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="portal-newsfeed-view-all"]').length > 0) {
+        cy.get('[data-test="portal-newsfeed-view-all"]')
+          .each(($el) => {
+            cy.wrap($el)
+              .should('have.attr', 'tabindex', '0');
+          });
+      }
+    });
+
+    // Test portal-newsfeed-item elements (if newsfeed items exist)
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="portal-newsfeed-item"]').length > 0) {
+        cy.get('[data-test="portal-newsfeed-item"]')
+          .each(($el) => {
+            cy.wrap($el)
+              .should('have.attr', 'tabindex', '-1');
+          });
+      }
+    });
+
+    // Test portal-quick-draft-entry elements (if quick links exist)
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="portal-quick-draft-entry"]').length > 0) {
+        cy.get('[data-test="portal-quick-draft-entry"]')
+          .each(($el) => {
+            cy.wrap($el)
+              .should('have.attr', 'tabindex', '-1');
+          });
+      }
+    });
+
+    // Test portal-corner-link elements (if corner links exist)
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="portal-corner-link"]').length > 0) {
+        cy.get('[data-test="portal-corner-link"]')
+          .each(($el) => {
+            cy.wrap($el)
+              .should('have.attr', 'tabindex', '-1');
+          });
+      }
+    });
+  });
+
+  it('every link and button should have tabindex 0 when a modal is opened and closed again', () => {
+    // Wait for page to fully load
+    cy.get('.portal-tile')
+      .should('exist')
+      .and('have.length.greaterThan', 0);
+
+    cy.get('[data-test="portal-folder"]').first()
+      .click();
+
+    // Wait for the modal to become visible and fully loaded
+    cy.get('.modal-wrapper--isVisible')
+      .should('exist')
+      .and('be.visible');
+
+    // Wait a bit for animations to complete
+    cy.wait(500);
+
+    // Close the modal using ESC key (most reliable method)
+    cy.get('body').type('{esc}');
+
+    // Alternative: If ESC doesn't work, try clicking on the modal backdrop
+    // cy.get('.modal-wrapper--isVisible').then(($modal) => {
+    //   const rect = $modal[0].getBoundingClientRect();
+    //   cy.get('.modal-wrapper--isVisible').click(rect.width - 10, 10);
+    // });
+
+    // Wait for modal to close
+    cy.get('.modal-wrapper--isVisible')
+      .should('not.exist');
+
+    cy.get('[data-test="tileLink"]')
+      .should('exist')
+      .not('.portal-tile--minified')
+      .each(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'tabindex', '0');
+      });
+
+    // Test portal-newsfeed-view-all elements (if newsfeed exists)
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="portal-newsfeed-view-all"]').length > 0) {
+        cy.get('[data-test="portal-newsfeed-view-all"]')
+          .each(($el) => {
+            cy.wrap($el)
+              .should('have.attr', 'tabindex', '0');
+          });
+      }
+    });
+
+    // Test portal-newsfeed-item elements (if newsfeed items exist)
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="portal-newsfeed-item"]').length > 0) {
+        cy.get('[data-test="portal-newsfeed-item"]')
+          .each(($el) => {
+            cy.wrap($el)
+              .should('have.attr', 'tabindex', '0');
+          });
+      }
+    });
+
+    // Test portal-quick-draft-entry elements (if quick links exist)
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="portal-quick-draft-entry"]').length > 0) {
+        cy.get('[data-test="portal-quick-draft-entry"]')
+          .each(($el) => {
+            cy.wrap($el)
+              .should('have.attr', 'tabindex', '0');
+          });
+      }
+    });
+
+    // Test portal-corner-link elements (if corner links exist)
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-test="portal-corner-link"]').length > 0) {
+        cy.get('[data-test="portal-corner-link"]')
+          .each(($el) => {
+            cy.wrap($el)
+              .should('have.attr', 'tabindex', '0');
+          });
+      }
+    });
+  });
 });
 /* eslint-enable jest/expect-expect */
