b828527c5fa04e088bf87b4086bcdcf21ff5a90c
Chrissy Topal <chrissy.topal.extern@univention.de>
2025-09-02 19:15:04 +0200

    chore(portal-frontend): wip_ fix tests univention/dev/projects/portal#743

--- debug-portal-filtered.js
+++ debug-portal-filtered.js
@@ -1,3 +1,8 @@
+/**
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * SPDX-FileCopyrightText: 2025 Univention GmbH
+ */
+
 // Simple debug for portalFinalLayoutFiltered
 // Paste this in browser console while the portal is running
 
